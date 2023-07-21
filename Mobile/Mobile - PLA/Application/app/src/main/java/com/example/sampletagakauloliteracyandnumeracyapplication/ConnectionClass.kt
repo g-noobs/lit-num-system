8efc1a03495571b630de6136797e0021303f1fdd
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-07-21 19:34:53 +0800

    deleted-mobile-folder
    deleted-mobile-folder

--- Mobile/Mobile - PLA/Application/app/src/main/java/com/example/sampletagakauloliteracyandnumeracyapplication/ConnectionClass.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sampletagakauloliteracyandnumeracyapplication
-
-fun ConnectionClass() : String {
-    val ipAddress = "http://25.29.203.47:8080/TTLN/MobileWeb/"
-    return ipAddress
-}
--- Mobile/Mobile - Profile n Topic/Mobile/Application/app/src/main/java/com/example/sampletagakauloliteracyandnumeracyapplication/ConnectionClass.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sampletagakauloliteracyandnumeracyapplication
-
-fun ConnectionClass() : String {
-    val ipAddress = "http://192.168.1.2:8080/TTLN/"
-    return ipAddress
-}
