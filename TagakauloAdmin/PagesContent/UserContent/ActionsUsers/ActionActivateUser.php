ac7f4b38462729f3e248dd949de045ac2049fc1a
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-08-22 16:37:27 +0800

    -updated-ActionActiveUser.php-PagesContentFolder

--- TagakauloAdmin/PagesContent/UserContent/ActionsUsers/ActionActivateUser.php
+++ TagakauloAdmin/PagesContent/UserContent/ActionsUsers/ActionActivateUser.php
@@ -2,15 +2,16 @@
 $table = "tbl_user_info";
 
 $id = $_POST['userId'];
+$status = 1;
 
 $sql = "UPDATE $table SET status_id = 1 WHERE user_info_id = '$id'";
 
 $query = "UPDATE $table SET status_id = ? WHERE user_info_id = ?";
-$params = [$id, $status];
+$params = [$status, $id];
 
 //still follow the usual tracing of php class
-include_once("../..../Database/SanitizeCrudClass.php");
-$activateUser = new SanitizeCrudClass();
+include_once("../../../Database/SanitizeCrudClass.php");
+$archive = new SanitizeCrudClass();
 $archive->executePreparedStatement($query, $params, "../../../pages/user.php");
 ?>
 
