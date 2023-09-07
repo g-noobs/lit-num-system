4634796b0474ef708642dc9a7b49829aa48bec1c
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-09-07 16:05:02 +0800

    work-on-adding-content

--- TagakauloAdmin/PagesContent/LessonContent/TopicFolder/LessonTopic.php
+++ TagakauloAdmin/PagesContent/LessonContent/TopicFolder/LessonTopic.php
@@ -1,22 +1,40 @@
 <br>
+<style>
+a {
+    color: inherit;
+    /* This makes links inherit their color from their parent elements */
+    text-decoration: none;
+    /* This removes the underline */
+}
+</style>
 <div class="container-fluid">
     <a href="#" type="button" id="back-table"><i class="glyphicon glyphicon-chevron-left"></i></a>
-
+    <h4> Add a new topic for lesson:</h4>
 </div>
 
+<div class="container-fluid">
 
-<div class="container">
-    <h4> Add a new topic for lesson:
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#">PDF</a></li>
+        <li><a href="#">Image</a></li>
+        <li><a href="#">Audio</a></li>
+        <li><a href="#">Video</a></li>
+    </ul>
 
-    </h4>
-</div>
-
-
-<div class="container">
-    <div class="btn-group">
-        <button type="button" class="btn btn-primary">Apple</button>
-        <button type="button" class="btn btn-primary">Samsung</button>
-        <button type="button" class="btn btn-primary">Sony</button>
+    <div class="panel panel-default">
+        <div class="panel-body">PDF CONTENT
+        </div>
     </div>
-
+    <div class="panel panel-default">
+        <div class="panel-body">IMAGE
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">AUDIO
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">VIDEO
+        </div>
+    </div>
 </div>
