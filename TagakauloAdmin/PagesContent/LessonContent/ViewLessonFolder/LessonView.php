5fad31fa5c735391e746ba6cc1db24e0652d0399
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-09-19 17:56:05 +0800

    view lesson update front-end

--- TagakauloAdmin/PagesContent/LessonContent/ViewLessonFolder/LessonView.php
+++ TagakauloAdmin/PagesContent/LessonContent/ViewLessonFolder/LessonView.php
@@ -1,16 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
+<html style="height: auto; min-height: 100%;">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View Lesson</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Teacher Portal</title>
+
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <?php include_once "../../../bootstrap/css.php"?>
-    <?php include_once("../../../CommonPHPClass/ModifiedDropdown.php");?>
+
+    <?php include_once "../ViewLessonFolder/lessonStyle.php";?>
 </head>
 
-<body class="sidebar-mini skin-yellow fixed" data-new-gr-c-s-check-loaded="14.1111.0" data-gr-ext-installed
-    style="height: 100%; min-height: 100%;">
-    
+<body class="skin-yellow layout-top-nav" style="height: auto; min-height: 100%;"
+    data-new-gr-c-s-check-loaded="14.1117.0" data-gr-ext-installed="">
+    <div class="wrapper" style="height: auto; min-height: 100%;">
+        <?php 
+            include_once "../ViewLessonFolder/ViewHeader.php";
+        ?>
+
+        <div class="content-wrapper" style="min-height: 606.2px;">
+            <div class="container">
+
+                <section class="content-header">
+                    <h1>
+                        Top Navigation
+                        <small>Example </small>
+                    </h1>
+                    <!-- Breadcrumb   
+                    <ol class="breadcrumb">
+                        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                        <li><a href="#">Layout</a></li>
+                        <li class="active">Top Navigation</li>
+                    </ol> -->
+                </section>
+
+                <!-- Main Content-->
+                <section class="content">
+                    <div class="box box-warning">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Blank Box</h3>
+                        </div>
+                        <div class="box-body">
+                            The great content goes here
+                        </div>
+
+                    </div>
+
+                </section>
+
+            </div>
+
+        </div>
+
+        <?php
+        include_once "../ViewLessonFolder/ViewFooter.php"; 
+        ?>
+    </div>
+    <?php include_once "../ViewLessonFolder/lessonQuery.php"?>
 </body>
+
+
 </html>
