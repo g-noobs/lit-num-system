41cb2758293ad32a790a81a6e784021ee502ada7
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-09-22 04:11:42 +0000

    Added Script for pulling and viewing content

--- TagakauloAdmin/PagesContent/LessonContent/TopicFolder/TopicScript.php
+++ TagakauloAdmin/PagesContent/LessonContent/TopicFolder/TopicScript.php
@@ -11,3 +11,68 @@
         });
     });
     </script>
+
+<!-- Script for pulling and viewing content-->
+<script>
+    $(function(){
+        var arrays = []; // Array to store arrays received from the backend
+        var currentArrayIndex = 0; // Index of the currently displayed array
+        var currentIndex = 0; // Index of the currently displayed item
+        var pageSize = 6; // Number of items to show per page
+
+        // Function to load data from the backend using AJAX
+        function loadData() {
+            $.ajax({
+                url: 'backend.php', // Replace with the actual backend URL
+                method: 'GET',
+                dataType: 'json',
+                success: function(data) {
+                    // Assume the backend returns data as an array of arrays
+                    arrays = data;
+                    loadPage(currentIndex);
+                },
+                error: function() {
+                    console.error('Failed to load data.');
+                }
+            });
+        }
+
+        // Function to load a page
+        function loadPage(index) {
+            var currentArray = arrays[currentArrayIndex];
+            if (index >= 0 && index < currentArray.length) {
+                var content = '<div class="media"><img src="' + currentArray[index] + '" alt="Image"></div>';
+                $('#gallery').html(content);
+            }
+        }
+        // Initial load
+        loadData();
+
+        // Handle next and previous clicks
+        $('#next').on('click', function() {
+            var currentArray = arrays[currentArrayIndex];
+            if (currentIndex < currentArray.length - 1) {
+                currentIndex++;
+                loadPage(currentIndex);
+            } else if (currentArrayIndex < arrays.length - 1) {
+                // Switch to the next array if available
+                currentArrayIndex++;
+                currentIndex = 0;
+                loadPage(currentIndex);
+            }
+        });
+
+        $('#prev').on('click', function() {
+            if (currentIndex > 0) {
+                currentIndex--;
+                loadPage(currentIndex);
+            } else if (currentArrayIndex > 0) {
+                // Switch to the previous array if available
+                currentArrayIndex--;
+                currentIndex = arrays[currentArrayIndex].length - 1;
+                loadPage(currentIndex);
+            }
+        });
+
+    });
+</script>
