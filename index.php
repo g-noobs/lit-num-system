fbc425235c1b8a5d7c2ef39ff84534421f30afd7
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-09-24 11:21:37 +0800

    End of Testing sync

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
     <div class="login-form">
         <form action="ActionValidateUser.php" method="post">
             <div class="avatar"><i class="material-icons">&#xE7FF;</i></div>
-            <h4 class="modal-title">Login to Your my man</h4>
+            <h4 class="modal-title">Login to Your Account</h4>
             <div class="form-group">
                 <input type="text" name="username" class="form-control" placeholder="Username" required="required">
             </div>
