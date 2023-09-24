a490f4fbb4e19176688559da0e33e95c10829ce3
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-09-24 19:39:31 +0800

    connection -ready

--- Database/Connection.php
+++ Database/Connection.php
@@ -9,9 +9,15 @@ class Connection{
     public function __construct(){
         $this->host = "srv1040.hstgr.io";  // Replace with your server name
         $this->username = "admin";  // Replace with your MySQL username
-        $this->password = "Capstone1!";  // Replace with your MySQL password
+        $this->password = "u170333284_admin";  // Replace with your MySQL password
         $this->database = "u170333284_db_tagakaulo";  // Replace with your database name
         $this->connect();
+
+        // $this->host = "localhost";  // Replace with your server name
+        // $this->username = "admin";  // Replace with your MySQL username
+        // $this->password = "admin";  // Replace with your MySQL password
+        // $this->database = "db_tagakaulo";  // Replace with your database name
+        // $this->connect();
     }
     public function connect(){
         $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->database);
