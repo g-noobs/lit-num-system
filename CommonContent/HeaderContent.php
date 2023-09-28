af42a95158882ece9b459b377e86ef6f6dae55a6
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-09-28 19:17:21 +0800

    Revert Login page

--- CommonContent/HeaderContent.php
+++ CommonContent/HeaderContent.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+if ($_SESSION['loggedin'] !== true) {
     // Redirect the user to the login page
     header('Location: ../index.php');
     exit;
--- ValidateCredsClass.php
+++ ValidateCredsClass.php
@@ -3,6 +3,7 @@ include_once("Database/Connection.php");
 
 class ValidateCredsClass extends Connection{
 
+
     function checkCreds($username,$password){
         $table = "view_userinfo_creds";
         $sql = "SELECT * FROM $table  WHERE uname = '$username' AND pass = '$password' AND user_level_id = 0 AND status_id = 1;";
@@ -16,7 +17,12 @@ class ValidateCredsClass extends Connection{
                 
             }
         }
+        else{
+            $message = 'Username or Password Error'; 
+             // Pass message as GET parameter
+            header('Location: index.php?msg=' . urlencode($message));
+            exit();
+        }
     }
 }
 ?>
-
