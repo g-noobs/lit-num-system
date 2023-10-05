237c8d4ed1974ecfdc0ffb2b970e584449f95cf6
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-10-05 03:33:44 +0000

    updateds

--- PagesContent/UserContent/CommonUser/ArchiveActivateUserScript.php
+++ PagesContent/UserContent/CommonUser/ArchiveActivateUserScript.php
@@ -5,11 +5,12 @@ $(document).ready(function() {
     // Click event for the edit icon
     $('.action').click(function() {
         // Get the row data
+        var userId = $(this).data('id');
         var fName = $(this).closest('tr').find('td:eq(3)').text();
         var lName = $(this).closest('tr').find('td:eq(4)').text();
 
         // get data-id from class action
-        var userId = $(this).data('id');
+        
 
         // assign the data to the id within the modal
         $('#arch_usr_id, #act_usr_id').text(userId);
