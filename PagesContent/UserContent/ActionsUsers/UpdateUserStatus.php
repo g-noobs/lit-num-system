81765e3002aceb1b909cfb8b3881b172a84f200d
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-10-05 04:11:03 +0000

    revert

--- PagesContent/UserContent/ActionsUsers/UpdateUserStatus.php
+++ PagesContent/UserContent/ActionsUsers/UpdateUserStatus.php
@@ -1,36 +1,28 @@
 <?php
-if (!empty($_POST['status']) && !empty($_POST['id'])) {
-    $table = "tbl_user_info";
-    $status = $_POST['status'];
-    $id = intval($_POST['id']);
+$table = "tbl_user_info";
+$status = $_POST['status'];
+$id = intval($_POST['id']);
 
-    $query = "UPDATE $table SET status_id = ? WHERE user_info_id = ?";
-    $params = [$status, $id];
+$query = "UPDATE $table SET status_id = ? WHERE user_info_id = ?";
+$params = [$status, $id];
 
-    include_once("../../../Database/SanitizeCrudClass.php");
-    $update = new SanitizeCrudClass();
-    $update->executePreState($query, $params);
+include_once("../../../Database/SanitizeCrudClass.php");
+$update = new SanitizeCrudClass();
+$update->executePreState($query, $params);
 
-    //Check and return true if the user is successfully archived
-    if($update){
-        if ($status == 0) {
-            $response = array('success' => 'User archived');
-        } else {
-            $response = array('success' => 'User activated');
-        }
-
-        echo json_encode($response);
+//Check and return true if the user is successfully archived
+if($update){
+    if ($status == 0) {
+        $response = array('success' => 'User archived');
+    } else {
+        $response = array('success' => 'User activated');
     }
-    else{
-        $response = array('error' => 'Error in updating status');
-        echo json_encode($response);
-    }
-    
-} else {
-    $response = array('error' => 'One or more POST fields are empty');
+
+    echo json_encode($response);
+}
+else{
+    $response = array('error' => 'Error in updating status');
     echo json_encode($response);
 }
 
-
-    
 ?>
