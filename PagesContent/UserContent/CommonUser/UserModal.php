81dd26290b28eb1e16198f89d507b6195a6d6f9e
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-10-05 04:54:26 +0000

    yyy

--- PagesContent/UserContent/CommonUser/UserModal.php
+++ PagesContent/UserContent/CommonUser/UserModal.php
@@ -157,7 +157,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button id="actvUsrBtn" class="btn btn-danger pull-left">Archive</button>
+                <button id="actvUsrBtn" class="btn btn-success pull-left">Archive</button>
                 <button type="reset" class="btn btn-default pull-right" data-dismiss="modal">Cancel</button>
             </div>
         </div>
