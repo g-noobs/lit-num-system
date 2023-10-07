e3fb9957d311e05bb771408af91c2ac60c76a398
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-07 17:49:37 +0800

    update category heading

--- PagesContent/CategoryFolder/CategoryTable.php
+++ PagesContent/CategoryFolder/CategoryTable.php
@@ -3,7 +3,7 @@
     <div class="col-xs-12">
         <div class="box">
             <div class="box-header">
-                <h3 class="box-title">Area List</h3>
+                <h3 class="box-title">Category List</h3>
             </div>
             <!-- /.box-header -->
             <div class="container-fluid">
