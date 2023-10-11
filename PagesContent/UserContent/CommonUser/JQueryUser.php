bcb114a4b35b030f4417330f9608fed1bc3c8dac
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-10-11 02:49:09 +0000

    updated Jquery for custom dropdown

--- PagesContent/UserContent/CommonUser/JQueryUser.php
+++ PagesContent/UserContent/CommonUser/JQueryUser.php
@@ -5,7 +5,7 @@
 // *This will disable the personal id input field if selected user is admin-->
 <script>
 $(document).ready(function() {
-    
+
     $('#user, #edit_user_option').on('change', function() {
         var selectedUser = $(this).val();
 
@@ -28,70 +28,75 @@ $(document).ready(function() {
 </script>
 
 <script>
-$(function(){
+$(function() {
     // $This manage the dropdown menu
-    $('.custom-dropdown-menu a').on('click',function(e) {
-        e.preventDefault();
-        var userType = $(this).data('user-type');
-        var contentPath = '';
+    $(function() {
+        // Cache the jQuery selectors
+        var $dropdownMenu = $('.custom-dropdown-menu');
+        var $mainContent = $('#mainContent');
+        var $modalTitle = $('#modal-title');
+        var $userType = $('#user_type');
+        var $personalId = $('#personal-id');
+        var $customDropdownToggle = $('.custom-dropdown-toggle');
 
-        
-        if (userType === 'all-active') {
+        // Use event delegation
+        $dropdownMenu.on('click', 'a', function(e) {
+            e.preventDefault();
+            var userType = $(this).data('user-type');
+            var contentPath = '';
 
-            contentPath = '../PagesContent/UserContent/UserTable/AllActiveUserTable.php';
-            $("#user").prop("disabled", false);
+            if (userType === 'all-active') {
+                contentPath = '../PagesContent/UserContent/UserTable/AllActiveUserTable.php';
+                $user.prop('disabled', false);
+            } else if (userType === 'teacher') {
+                $personalId.prop('disabled', false);
+                contentPath = '../PagesContent/UserContent/UserTable/TeacherTableContent.php';
+                updatModalForm('Teacher');
+            } else if (userType === 'learner') {
+                $personalId.prop('disabled', false);
+                contentPath = '../PagesContent/UserContent/UserTable/StudentTableContent.php';
+                updatModalForm('Learner');
+            } else if (userType === 'admin') {
+                contentPath = '../PagesContent/UserContent/UserTable/AdminTableContent.php';
+                $personalId.prop('disabled', true);
+                updatModalForm('Admin');
+            } else if (userType === 'arch-all') {
+                contentPath = '../PagesContent/UserContent/UserTable/AllArchUsersTable.php';
+            } else if (userType === 'arch-admin') {
+                contentPath = '../PagesContent/UserContent/UserTable/ArchiveAdminTable.php';
+            } else if (userType === 'arch-teacher') {
+                contentPath = '../PagesContent/UserContent/UserTable/ArchivedTeacherTable.php';
+            } else if (userType === 'arch-learner') {
+                contentPath = '../PagesContent/UserContent/UserTable/ArchivedStudentTable.php';
+            }
 
+            $customDropdownToggle.html($(this).text() + '<span class="caret"></span>');
 
-        } else if (userType === 'teacher') {
-            $('#personal-id').prop("disabled", false);
-            contentPath = '../PagesContent/UserContent/UserTable/TeacherTableContent.php';
-            updatModalForm('Teacher');
-
-        } else if (userType === 'learner') {
-            $('#personal-id').prop("disabled", false);
-            contentPath = '../PagesContent/UserContent/UserTable/StudentTableContent.php';
-            updatModalForm('Learner');
-
-        } else if (userType === 'admin') {
-            contentPath = '../PagesContent/UserContent/UserTable/AdminTableContent.php';
-            $('#personal-id').prop("disabled", true);
-            updatModalForm('Admin');
-
-
-        } else if (userType === 'arch-all') {
-            contentPath = '../PagesContent/UserContent/UserTable/AllArchUsersTable.php';
-        } else if (userType === 'arch-admin') {
-            contentPath = '../PagesContent/UserContent/UserTable/ArchiveAdminTable.php';
-        } else if (userType === 'arch-teacher') {
-            contentPath = '../PagesContent/UserContent/UserTable/ArchivedTeacherTable.php';
-        } else if (userType === 'arch-learner') {
-            contentPath = '../PagesContent/UserContent/UserTable/ArchivedStudentTable.php';
-        }
-        $('.custom-dropdown-toggle').html($(this).text() + '<span class="caret"></span>');
-        if (contentPath !== '') {
-            $("#mainContent").fadeOut(400, function() {
-                $(this).empty().load(contentPath, function() {
-                    $(this).fadeIn(400);
+            if (contentPath !== '') {
+                $mainContent.fadeOut(400, function() {
+                    $(this).empty().load(contentPath, function() {
+                        $(this).fadeIn(400);
+                    });
                 });
-            });
+            }
+        });
+
+        function updatModalForm(usertype) {
+            // Modal title
+            $modalTitle.text('Enter ' + usertype + ' Information');
+            $userType.val(usertype);
+            // Disable the select element
+            $userType.prop('disabled', true);
         }
     });
-
-    function updatModalForm(usertype) {
-        // Modal title
-        $('#modal-title').text('Enter ' + usertype + ' Information');
-        $("#user_type").val(usertype);
-        // Disable the select element
-        $("#user_type").prop("disabled", true);
-    }
 });
 </script>
 
 <!-- CSV for hiding and showing upload batch form -->
 <script>
-$(function(){
+$(function() {
     $('#frmCsvGroup').hide();
-    $('#csvUploadIcon').click(function(){
+    $('#csvUploadIcon').click(function() {
         $('#frmCsvGroup').fadeToggle("slow");
     });
 });
