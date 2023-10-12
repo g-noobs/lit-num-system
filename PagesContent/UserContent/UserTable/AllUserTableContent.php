8451d31b8cdd41deb3ea24870b145284b77e8ba6
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-10-12 02:01:04 +0000

    fix thead

--- PagesContent/UserContent/UserTable/AllUserTableContent.php
+++ PagesContent/UserContent/UserTable/AllUserTableContent.php
@@ -2,8 +2,8 @@
     .table thead {
         position: sticky;
         top: 0;
-        background-color: #fff;
-        border-bottom: 1px solid #ccc;
+        background-color: gray;
+        bordered: 1px solid #ccc;
         /* background-color: #fff; Set your preferred background color */
     }
 </style>
