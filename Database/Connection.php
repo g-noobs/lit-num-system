9905d125c212e07474962d106c5330aa1bc3c975
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-15 08:00:31 +0800

    check hostss

--- Database/Connection.php
+++ Database/Connection.php
@@ -1,40 +1,45 @@
 <?php
-class Connection{
-    protected $host;
+class Connection {
+    protected $primaryHost;
+    protected $backupHost;
     protected $username;
     protected $password;
     protected $database;
     public $conn;
 
-    public function __construct(){
-        //! This bellow will be used for online deployment
-        $this->host = "156.67.222.1";  // Replace with your server name
-        $this->username = "u170333284_admin";  // Replace with your MySQL username
+    public function __construct() {
+        $this->primaryHost = "156.67.222.1"; // Primary host (online deployment)
+        $this->backupHost = "localhost:3306";  // Backup host (local)
+        $this->username = "u170333284_admin"; // Replace with your MySQL username
         $this->password = "Capstone1!";  // Replace with your MySQL password
-        $this->database = "u170333284_db_tagakaulo";  // Replace with your database name
+        $this->database = "u170333284_db_tagakaulo"; // Replace with your database name
         $this->connect();
-
-        // $this->host = "localhost:3306";  // Replace with your server name
-        // $this->username = "admin";  // Replace with your MySQL username
-        // $this->password = "admin";  // Replace with your MySQL password
-        // $this->database = "db_tagakaulo";  // Replace with your database name
-        // $this->connect();
     }
-    public function connect(){
-        $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->database);
+
+    public function connect() {
+        $this->conn = mysqli_connect($this->primaryHost, $this->username, $this->password, $this->database);
+
         if (!$this->conn) {
-            die("Connection failed: " . mysqli_connect_error());
+            // Primary host connection failed; try the backup host
+            $this->conn = mysqli_connect($this->backupHost, $this->username, $this->password, $this->database);
+
+            if (!$this->conn) {
+                die("Connection failed for both primary and backup hosts: " . mysqli_connect_error());
+            }
         }
     }
-    //This will close the Connection
-    public function close(){
-        if($this->conn){
+
+    // This will close the Connection
+    public function close() {
+        if ($this->conn) {
             $this->conn->close();
         }
     }
-    //This will be used to get connection once $conn is set to protected
-    public function getConnection(){
+
+    // This will be used to get the connection once $conn is set to protected
+    public function getConnection() {
         return $this->conn;
     }
 }
+
 ?>
