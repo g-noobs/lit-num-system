33cb1169902eb40759199aa79d93d674f5247ce4
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-15 14:30:21 +0800

    test

--- Database/QuizDisplayClass.php
+++ Database/QuizDisplayClass.php
@@ -6,11 +6,36 @@ class QuizDisplayClass extends Connection{
         parent :: __construct();
     }
     function quizTable(){
-        $sql = "SELECT * FROM combined_quiz_view";
+        $sql = "SELECT * FROM view_answer_question_quiz";
         $result = $this->conn->query($sql);
 
         if($result->num_rows > 0){
             while($row = $result->fetch_assoc()){
+                echo "<tr>";
+
+                echo "<td>" ."". "</td>";
+                
+                echo "<td>".$row['quiz_id']. "</td>";
+                echo "<td>"."". "</td>";
+                echo "<td>"."". "</td>";;
+                echo "<td>".$row['question_id']. "</td>";
+                echo "<td>".$row['quiz_id']. "</td>";
+                echo "<td>".$row['question_text']. "</td>";
+                echo "<td>".$row['answer_id']. "</td>";
+                echo "<td>".$row['answer_text']. "</td>";
+
+                if ($row['is_correct'] == 1){
+                    echo "<td>"."Correct Answer". "</td>";
+                }
+                else{
+                    echo "<td>"."Wrong Answer". "</td>";
+                }
+
+                echo "<td>".$row['topic_id']. "</td>";
+                echo "<td>"."". "</td>";
+
+
+                echo "</tr>";
 
             }
         }
