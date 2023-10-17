26ea5946aa32ab708873c3052469dcfc851a5740
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-17 18:21:29 +0800

    asd

--- PagesContent/QuizFolder/TableQuiz/AddQuizSection.php
+++ PagesContent/QuizFolder/TableQuiz/AddQuizSection.php
@@ -33,23 +33,26 @@
                                 <input type="text" name="quiz_answer" id="quiz_answer" class="form-control" required>
                             </div>
                             <div class="row">
-                                <div class="form-group">
-                                    <label for="option1">Option 1</label>
-                                    <input type="text" name="option1">
+                                <div class="col-xs-6">
+                                    <div class="form-group">
+                                        <label for="option1">Option 1</label>
+                                        <input type="text" name="option1">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="option2">Option 2</label>
+                                        <input type="text" name="option2">
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="option2">Option 2</label>
-                                    <input type="text" name="option2">
-                                </div>
+                                <div class="col-xs-6">
+                                    <div class="form-group">
+                                        <label for="option3">Option 3</label>
+                                        <input type="text" name="option3">
+                                    </div>
 
-                                <div class="form-group">
-                                    <label for="option3">Option 3</label>
-                                    <input type="text" name="option3">
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="option4">Option 4</label>
-                                    <input type="text" name="option4">
+                                    <div class="form-group">
+                                        <label for="option4">Option 4</label>
+                                        <input type="text" name="option4">
+                                    </div>
                                 </div>
                             </div>
                         </div>
