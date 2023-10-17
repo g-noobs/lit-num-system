41adc9a76a857aaf1def387a20ef22343f294482
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-10-17 06:00:46 +0000

    xx

--- PagesContent/QuizFolder/TableQuiz/AddQuizSection.php
+++ PagesContent/QuizFolder/TableQuiz/AddQuizSection.php
@@ -4,7 +4,8 @@
             <div class="box-header with-border">
                 <h3 class="box-title"></h3>
                 <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-times"></i></button>
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
                 </div>
             </div>
             <br>
