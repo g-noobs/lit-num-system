ba0d99a6d31eca1103e64d1d17a20a8064520956
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-17 17:33:07 +0800

    xx

--- PagesContent/QuizFolder/TableQuiz/QuizMainTable.php
+++ PagesContent/QuizFolder/TableQuiz/QuizMainTable.php
@@ -3,14 +3,15 @@
         <div class="box box-warning container">
             <div class="box-header with-border">
                 <h3 class="box-title">All Quiz List</h3>
-
                 <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                    </button>
                     <div class="search-box" style="margin-right: 35px;">
                         <i class="fa fa-search"></i>
                         <input type="text" id="userInputTopic" class="form-control" placeholder="Search..">
                     </div>
+                </div>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
                 </div>
             </div>
             <br>
