259a98d4cec09dacb19f113c68b1af890e9a4329
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-17 20:25:10 +0800

    zz

--- PagesContent/QuizFolder/TableQuiz/AddQuizSection.php
+++ PagesContent/QuizFolder/TableQuiz/AddQuizSection.php
@@ -2,7 +2,7 @@
     <div class="col-xs-12">
         <div class="box box-warning container">
             <div class="box-header with-border">
-                <h3 class="box-title"></h3>
+                <h3 class="box-title">Add new Quiz Question</h3>
                 <div class="box-tools pull-right">
                     <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                     </button>
