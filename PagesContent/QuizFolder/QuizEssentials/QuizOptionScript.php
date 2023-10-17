9420314228d39d8b673d570d050ac0f2896f3846
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-17 20:54:54 +0800

    zx

--- PagesContent/QuizFolder/QuizEssentials/QuizOptionScript.php
+++ PagesContent/QuizFolder/QuizEssentials/QuizOptionScript.php
@@ -6,15 +6,15 @@ $(function() {
         var trueFalse = "";
         var multipleChoice = "";
 
-        if(selectedoption === 0){
+        if(selectedoption == 0){
             $("#set_answer_col").empty();
             $('#answer_options').append();
         }
-        else if(selectedoption === 1){
+        else if(selectedoption == 1){
             $("#answer_options").empty();
             $('#answer_options').append();
         }
-        else if(selectedoption === 2){
+        else if(selectedoption == 2){
             $("#answer_options").empty();                                
             $('#answer_options').append();
         }
