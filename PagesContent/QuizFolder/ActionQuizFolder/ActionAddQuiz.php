4462610f1fec79098cbe723289c4345657ba82bb
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-10-19 09:52:14 +0000

    push origin

--- PagesContent/QuizFolder/ActionQuizFolder/ActionAddQuiz.php
+++ PagesContent/QuizFolder/ActionQuizFolder/ActionAddQuiz.php
@@ -1,6 +1,20 @@
 <?php 
 session_start();
-
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(isset($_POST['quiz_question'])){
+        $values = array(
+            'quiz_id' => '',
+            'quiz_question' => $_POST['quiz_question'],
+            'quiz_selectionA' => '', //Correct Answer
+            'quiz_selectionB' => '',
+            'quiz_selectionC' => '',
+            'quiz_selectionD' => '',
+            'date_created' => '',
+            'topic_id' => $_POST['topic_id']
+        );
+        
+    }
+}
 // db colomnlist
 // quiz_id
 // quiz_question
