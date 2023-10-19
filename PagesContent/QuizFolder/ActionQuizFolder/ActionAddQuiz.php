b6fa57759acc4be918e2e2fc177180afa94d7764
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-19 20:24:21 +0800

    zxc

--- PagesContent/QuizFolder/ActionQuizFolder/ActionAddQuiz.php
+++ PagesContent/QuizFolder/ActionQuizFolder/ActionAddQuiz.php
@@ -1,5 +1,5 @@
 <?php 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
     if(isset($_POST['quiz_question'])){
         $values = array(
             'quiz_id' => '',
