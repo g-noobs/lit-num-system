cf125994aed926adf2bbb361102b878421123e28
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-22 19:27:36 +0800

    fgh

--- PagesContent/ReportsFolder/ActionReportFolder/ExportDataAction.php
+++ PagesContent/ReportsFolder/ActionReportFolder/ExportDataAction.php
@@ -1,13 +1,36 @@
 <?php
-if(isset($_POST["tableData"])) {
+if (isset($_POST["tableData"])) {
     $tableData = $_POST["tableData"];
-    $filename = "table_data.xls";
+    $filename = $_POST["fileName"];
 
-    // Set the appropriate headers for Excel download
-    header("Content-Type: application/vnd.ms-excel");
+    // Create an XML document from the table data (you will need to modify this part to suit your data structure)
+    $xml = new SimpleXMLElement('<quiz_data></quiz_data>');
+    // Assuming your table rows represent quiz questions, you can loop through them to create XML elements
+    // Modify this loop to match your data structure
+    foreach ($tableData as $row) {
+        $question = $xml->addChild('question');
+        $question->addChild('question_text', $row['question_text']);
+        $question->addChild('answer', $row['answer']);
+    }
+
+    // Set the appropriate headers for XML download
+    header('Content-Type: application/xml');
     header("Content-Disposition: attachment; filename=$filename");
 
-    // Output the table data
-    echo $tableData;
+    // Output the XML data
+    echo $xml->asXML();
+
+    // Return a JSON response with the file URL and name
+    $response = array(
+        'success' => true,
+        'fileUrl' => 'data:application/xml,' . urlencode($xml->asXML()),
+        'fileName' => $filename,
+    );
+
+    echo json_encode($response);
+} else {
+    // Return an error response if data is missing
+    $response = array('success' => false);
+    echo json_encode($response);
 }
 ?>
--- PagesContent/ReportsFolder/ScriptReportFolder/ExportScript.php
+++ PagesContent/ReportsFolder/ScriptReportFolder/ExportScript.php
@@ -1,21 +1,41 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 
 <script>
 $(document).ready(function() {
     $('#dataTable').DataTable();
 });
-</script>
+</script> -->
 
 <script>
 $(document).ready(function() {
     $('#exportButton').on('click', function() {
+        // Retrieve the table data as HTML
         var tableData = $('#dataTable').html();
-
-        // Send the table data to the export.php script
-        $.post('../PagesContent/ReportsFolder/ActionReportFolder/ExportDataAction.php', { tableData: tableData }, function(response) {
-            // Trigger the file download
-            window.location = 'data:application/vnd.ms-excel,' + encodeURIComponent(response);
+        var fileName = 'quiz_data.xml';
+        
+        // Send the table data to the server using AJAX
+        $.ajax({
+            method: 'POST',
+            url: 'export.php', // Create an export.php script to handle XML export
+            data: { tableData: tableData,
+                    fileName: fileName },
+            dataType: 'json',
+            success: function(response) {
+                // Check if the export was successful
+                if (response.success) {
+                    // Trigger the file download
+                    var link = document.createElement('a');
+                    link.href = response.fileUrl;
+                    link.download = response.fileName;
+                    link.click();
+                } else {
+                    alert('Export failed.');
+                }
+            },
+            error: function() {
+                alert('Export failed.');
+            }
         });
     });
 });
