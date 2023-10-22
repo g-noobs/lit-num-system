2c54aeb7ba27478f621c3f351b71dd88d72be3a9
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-22 19:59:09 +0800

    vv

--- PagesContent/ReportsFolder/ScriptReportFolder/ExportScript.php
+++ PagesContent/ReportsFolder/ScriptReportFolder/ExportScript.php
@@ -1,3 +1,6 @@
+
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+
 <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/gh/linways/table-to-excel@v1.0.4/dist/tableToExcel.js"></script>
 <script>
