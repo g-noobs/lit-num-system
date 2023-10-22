dac477ab68bd2da765db42b2b16e29f77a830b46
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-22 19:35:30 +0800

    asd

--- PagesContent/ReportsFolder/ActionReportFolder/ExportDataAction.php
+++ PagesContent/ReportsFolder/ActionReportFolder/ExportDataAction.php
@@ -2,29 +2,19 @@
 if (isset($_POST["tableData"])) {
     $tableData = $_POST["tableData"];
     $filename = $_POST["fileName"];
-
-    // Create an XML document from the table data (you will need to modify this part to suit your data structure)
-    $xml = new SimpleXMLElement('<quiz_data></quiz_data>');
-    // Assuming your table rows represent quiz questions, you can loop through them to create XML elements
-    // Modify this loop to match your data structure
-    foreach ($tableData as $row) {
-        $question = $xml->addChild('question');
-        $question->addChild('question_text', $row['question_text']);
-        $question->addChild('answer', $row['answer']);
-    }
-
-    // Set the appropriate headers for XML download
-    header('Content-Type: application/xml');
+    
+    // Set the appropriate headers for Excel download
+    header("Content-Type: application/vnd.ms-excel");
     header("Content-Disposition: attachment; filename=$filename");
 
-    // Output the XML data
-    echo $xml->asXML();
+    // Output the table data
+    echo $tableData;
 
     // Return a JSON response with the file URL and name
     $response = array(
         'success' => true,
-        'fileUrl' => 'data:application/xml,' . urlencode($xml->asXML()),
-        'fileName' => $filename,
+        'fileUrl' => "export.php",
+        'fileName' => $filename
     );
 
     echo json_encode($response);
--- PagesContent/ReportsFolder/ScriptReportFolder/ExportScript.php
+++ PagesContent/ReportsFolder/ScriptReportFolder/ExportScript.php
@@ -1,25 +1,23 @@
-<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 
 <script>
 $(document).ready(function() {
     $('#dataTable').DataTable();
 });
-</script> -->
+</script>
 
 <script>
 $(document).ready(function() {
     $('#exportButton').on('click', function() {
-        // Retrieve the table data as HTML
         var tableData = $('#dataTable').html();
-        var fileName = 'quiz_data.xml';
+        var fileName = "table_data.xls";
         
-        // Send the table data to the server using AJAX
         $.ajax({
             method: 'POST',
-            url: '../PagesContent/ReportsFolder/ActionReportFolder/ExportDataAction.php', // Create an export.php script to handle XML export
+            url: '../PagesContent/ReportsFolder/ActionReportFolder/ExportDataAction.php',
             data: { tableData: tableData,
-                    fileName: fileName },
+                    fileName: fileName},
             dataType: 'json',
             success: function(response) {
                 // Check if the export was successful
@@ -40,5 +38,3 @@ $(document).ready(function() {
     });
 });
 </script>
-
-
