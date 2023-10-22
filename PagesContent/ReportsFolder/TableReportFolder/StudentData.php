540b87f190222137e370044398ef5d7bc80ca97a
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-22 19:40:07 +0800

    thead issue

--- PagesContent/ReportsFolder/TableReportFolder/StudentData.php
+++ PagesContent/ReportsFolder/TableReportFolder/StudentData.php
@@ -24,7 +24,12 @@
                 <table id="dataTable" class="table table-bordered table-hover">
                     <thead>
                         <tr>
-                            <?php include_once "QuizTableHeader.php";?>
+                            <th></th>
+                            <th>Quiz ID</th>
+                            <th>Question</th>
+                            <th>Date Created</th>
+                            <th>Topic ID</th>
+                            <th colspan=2>Action</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -46,4 +51,3 @@
 
 <!-- Common Script with other pages -->
 <?php include_once "../../../CommonContent/CommonAllScript.php"?>
-
