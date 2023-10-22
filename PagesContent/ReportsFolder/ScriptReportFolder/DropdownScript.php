2653be3a3aa7dbd8f4f3e41b2f821eacb2c8c00e
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-22 21:01:04 +0800

    zfsd

--- PagesContent/ReportsFolder/ScriptReportFolder/DropdownScript.php
+++ PagesContent/ReportsFolder/ScriptReportFolder/DropdownScript.php
@@ -2,20 +2,20 @@
 $(document).ready(function() {
     $('.custom-dropdown-menu a').click(function(e) {
         e.preventDefault();
-        var userType = $(this).data('report');
+        var dataType = $(this).data('report');
         var contentPath = '';
 
-        if (userType === 'student-data') {
+        if (dataType === 'student-data') {
             contentPath = '../PagesContent/ReportsFolder/TableReportFolder/StudentData.php';
-        } else if (userType === 'teacher-data') {
+        } else if (dataType === 'teacher-data') {
             contentPath = '../PagesContent/ReportsFolder/TableReportFolder/TeacherData.php';
-        } else if (userType === 'subject-data') {
+        } else if (dataType === 'subject-data') {
             contentPath = '../PagesContent/ReportsFolder/TableReportFolder/SubjectData.php';
-        }else if (userType === 'lesson-data') {
+        }else if (dataType === 'lesson-data') {
             contentPath = '../PagesContent/ReportsFolder/TableReportFolder/LessonData.php';
-        } else if (userType === 'topic-data') {
+        } else if (dataType === 'topic-data') {
             contentPath = '../PagesContent/ReportsFolder/TableReportFolder/TopicData.php';
-        }else if (userType === 'quiz-data') {
+        }else if (dataType === 'quiz-data') {
             contentPath = '../PagesContent/ReportsFolder/TableReportFolder/QuizData.php';
         } 
         $('.custom-dropdown-toggle').html($(this).text() + '<span class="caret"></span>');
