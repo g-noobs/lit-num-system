8cd3a3115cb7eb4637902a6bd61c3af2ae9b4341
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-22 19:20:33 +0800

    xx

--- PagesContent/ReportsFolder/ScriptReportFolder/ExportScript.php
+++ PagesContent/ReportsFolder/ScriptReportFolder/ExportScript.php
@@ -1,4 +1,5 @@
-
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 
 <script>
 $(document).ready(function() {
