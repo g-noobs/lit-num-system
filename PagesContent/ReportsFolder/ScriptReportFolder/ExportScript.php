20e8efb39fd6fd684acd9ee7259e7ff2269c3c9a
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-22 19:04:27 +0800

    added export script

--- PagesContent/ReportsFolder/ScriptReportFolder/ExportScript.php
+++ PagesContent/ReportsFolder/ScriptReportFolder/ExportScript.php
@@ -1,3 +1,4 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 <script>
 $(document).ready(function() {
