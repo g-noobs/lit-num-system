55440d1196d40f5f20c66fd51c780dab83a0aedc
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-10-23 05:18:34 +0000

    From Append to Exit

--- PagesContent/UserContent/CommonUser/ScriptViewUserData.php
+++ PagesContent/UserContent/CommonUser/ScriptViewUserData.php
@@ -11,15 +11,15 @@ $(function(){
             success: function(response){
                 var responseData = JSON.parse(response);
 
-                $('#personal_id_data').append(responseData.personal_id);
-                $('#full_name_data').append(responseData.full_name_data);
-                $('#gender_data').append(responseData.gender_data);
-                $('#email_data').append(responseData.email_data);
-                $('#birthdate_data').append(responseData.birthdate_data);
-                $('#user_type_data').append(responseData.user_type_data);
-                $('#user_status_data').append(responseData.user_status_data);
-                $('#username_data').append(responseData.username_data);
-                $('#password_data').append(responseData.password_data);
+                $('#personal_id_data').text(responseData.personal_id);
+                $('#full_name_data').text(responseData.full_name_data);
+                $('#gender_data').text(responseData.gender_data);
+                $('#email_data').text(responseData.email_data);
+                $('#birthdate_data').text(responseData.birthdate_data);
+                $('#user_type_data').text(responseData.user_type_data);
+                $('#user_status_data').text(responseData.user_status_data);
+                $('#username_data').text(responseData.username_data);
+                $('#password_data').text(responseData.password_data);
             },
             error:function(){
                 console.log('error');
