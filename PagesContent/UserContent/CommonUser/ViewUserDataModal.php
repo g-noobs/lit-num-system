e9d9dd7d4bf9d74fcb4479a0a9b361833bce38d4
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-10-23 05:20:32 +0000

    ad

--- PagesContent/UserContent/CommonUser/ViewUserDataModal.php
+++ PagesContent/UserContent/CommonUser/ViewUserDataModal.php
@@ -12,15 +12,15 @@
                 <div class="box box-widget widget-user-2">
                     <div class="box-footer no-padding">
                         <ul class="nav nav-stacked">
-                            <li><strong>Personal ID: <p id="personal_id_data"></p></strong> </li>
-                            <li><strong>Full Name: <p id="full_name_data"></p></strong> </li>
-                            <li><strong>Gender: <p id="gender_data"></p></strong></li>
-                            <li><strong>Email: <p id="email_data"></p></strong></li>
-                            <li><strong>Birthday: <p id="birthdate_data"></p></strong></li>
-                            <li><strong>User Type: <p id="user_type_data"></p></strong></li>
-                            <li><strong>User Status: <p id="user_status_data"></p></strong></li>
-                            <li><strong>Username: <p id="username_data"></p></strong></li>
-                            <li><strong>Password: <p id="password_data"></p></strong></li>
+                            <li><strong>Personal ID:</strong><p id="personal_id_data"></p> </li>
+                            <li><strong>Full Name:</strong><p id="full_name_data"></p> </li>
+                            <li><strong>Gender:</strong><p id="gender_data"></p></li>
+                            <li><strong>Email:</strong><p id="email_data"></p></li>
+                            <li><strong>Birthday:</strong><p id="birthdate_data"></p></li>
+                            <li><strong>User Type:</strong><p id="user_type_data"></p></li>
+                            <li><strong>User Status:</strong><p id="user_status_data"></p></li>
+                            <li><strong>Username:</strong><p id="username_data"></p></li>
+                            <li><strong>Password:</strong><p id="password_data"></p></li>
                         </ul>
                     </div>
                 </div>
