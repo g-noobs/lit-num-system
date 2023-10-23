dea71211e72bd08e658cc8fe8d010ac70da1d181
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-10-23 03:44:17 +0000

    test modal

---
+++ PagesContent/UserContent/CommonUser/ViewUserDataModal.php
@@ -0,0 +1,48 @@
+<div class="modal fade" id="user_data_modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" id="user_id_data"">Enter user Information</h4>
+            </div>
+            <!-- /.modal-header -->
+            <div class=" modal-body">
+                    <div class="box-body">
+
+                        <div class="box box-widget widget-user-2">
+
+                            <div class="widget-user-header bg-yellow">
+                                <div class="widget-user-image">
+                                    <img class="img-circle" src="../dist/img/user7-128x128.jpg" alt="User Avatar">
+                                </div>
+
+                                <h3 class="widget-user-username">Nadia Carmichael</h3>
+                                <h5 class="widget-user-desc">Lead Developer</h5>
+                            </div>
+                            <div class="box-footer no-padding">
+                                <ul class="nav nav-stacked">
+                                    <li><a href="#">Projects <span class="pull-right badge bg-blue">31</span></a></li>
+                                    <li><a href="#">Tasks <span class="pull-right badge bg-aqua">5</span></a></li>
+                                    <li><a href="#">Completed Projects <span
+                                                class="pull-right badge bg-green">12</span></a></li>
+                                    <li><a href="#">Followers <span class="pull-right badge bg-red">842</span></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <!-- /.box-body -->
+            </div>
+            <!-- /.modal-body -->
+            <div class="modal-footer">
+                <button type="reset" class="btn btn-default pull-left" data-dismiss="modal">Cancel</button>
+            </div>
+            <!-- /.modal-footer -->
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
