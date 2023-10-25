81864e4348da48348d0482560dd40a7565fb7719
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-25 20:49:19 +0800

    cvb

--- PagesContent/LessonContent/ViewLessonFolder/ViewLessonScript.php
+++ PagesContent/LessonContent/ViewLessonFolder/ViewLessonScript.php
@@ -10,9 +10,8 @@ $(function() {
     $("#lesson_name").text(lessonName);
 
     var responseData = {}; // To store the response from the backend
-    var currentArrayIndex = 0; // Index of the currently displayed topic
-    var currentIndex = 0; // Index of the currently displayed item
-    var pageSize = 6; // Number of items to show per page
+    var topics = []; // To store the topic keys
+    var currentTopicIndex = 0; // Index of the currently displayed topic
 
     // Function to load data from the backend using AJAX
     function loadData() {
@@ -22,15 +21,9 @@ $(function() {
             data: { id: lessonid }, // Pass any required data to your PHP script
             dataType: 'json',
             success: function(response) {
-                responseData = response; // Assuming the backend returns data as an object
-                loadPage(currentIndex);
-                // var responseDataJson = JSON.parse(response);
-                // if(responseDataJson.hasOwnProperty('error')){
-                //     var msg = responseDataJson.error;
-                //     $('#errorAlert').text(msg);
-                //     $('#errorBanner').show();
-                // }
-
+                responseData = response;
+                topics = Object.keys(response);
+                loadTopic(currentTopicIndex);
             },
             error: function() {
                 console.error('Failed to load data.');
@@ -38,51 +31,50 @@ $(function() {
         });
     }
 
-    // Function to load a page
-    function loadPage(index) {
-        if (responseData[currentArrayIndex]) {
-            var currentArray = responseData[currentArrayIndex];
-            if (index < currentArray.length) {
-                var currentItem = currentArray[index];
+    // Function to load a topic
+    function loadTopic(index) {
+        var currentTopic = topics[index];
+        var mediaPaths = responseData[currentTopic];
 
-                // Determine the file type based on the file extension
-                var fileType = currentItem.split('.').pop().toLowerCase();
+        if (mediaPaths) {
+            var content = '';
 
-                var content = '';
+            mediaPaths.forEach(function(mediaPath) {
+                var fileType = mediaPath.split('.').pop().toLowerCase();
 
                 if (fileType === 'jpg' || fileType === 'jpeg' || fileType === 'png' || fileType === 'gif') {
-                    content = '<div class="media"><img src="' + currentItem + '" alt="Image"></div>';
+                    content += '<div class="media"><img src="' + mediaPath + '" alt="Image"></div>';
                 } else if (fileType === 'mp3' || fileType === 'ogg' || fileType === 'wav') {
-                    content = '<audio controls><source src="' + currentItem + '" type="audio/' + fileType + '">Your browser does not support the audio element.</audio>';
+                    content += '<audio controls><source src="' + mediaPath + '" type="audio/' + fileType + '">Your browser does not support the audio element.</audio>';
                 } else if (fileType === 'mp4' || fileType === 'webm' || fileType === 'ogg') {
-                    content = '<video controls><source src="' + currentItem + '" type="video/' + fileType + '">Your browser does not support the video element.</video>';
+                    content += '<video controls><source src="' + mediaPath + '" type="video/' + fileType + '">Your browser does not support the video element.</video>';
                 } else if (fileType === 'pdf') {
-                    content = '<embed src="' + currentItem + '" type="application/pdf" width="100%" height="500px" />';
+                    content += '<embed src="' + mediaPath + '" type="application/pdf" width="100%" height="500px" />';
                 } else {
                     // Handle other file types or provide a default
-                    content = '<p>Unsupported file type</p>';
+                    content += '<p>Unsupported file type</p>';
                 }
+            });
 
-                $('#gallery').html(content);
-            }
+            $('#gallery').html(content);
         }
     }
 
     // Initial load
     loadData();
 
-    // Handle next and previous clicks
+    // Handle next and previous topic clicks
     $('#next').on('click', function() {
-        if (currentIndex < responseData[currentArrayIndex].length - 1) {
-            currentIndex++;
-            loadPage(currentIndex);
+        if (currentTopicIndex < topics.length - 1) {
+            currentTopicIndex++;
+            loadTopic(currentTopicIndex);
         }
     });
 
     $('#prev').on('click', function() {
-        if (currentIndex > 0) {
-            currentIndex--;
-            loadPage(currentIndex);
+        if (currentTopicIndex > 0) {
+            currentTopicIndex--;
+            loadTopic(currentTopicIndex);
         }
     });
 });
