182e5d8eb21789ba6f23336dd0354722c5099811
glen-caps2 <149028843+glen-caps2@users.noreply.github.com>
2023-10-26 02:37:27 +0000

    updates quiz

--- Database/QuizDisplayClass.php
+++ Database/QuizDisplayClass.php
@@ -13,10 +13,6 @@ class QuizDisplayClass extends Connection{
             while($row = $result->fetch_assoc()){
                 echo "<tr>";
 
-                echo "<td>" ."". "</td>";
-                
-                echo "<td>".$row['quiz_id']. "</td>";
-                echo "<td>"."". "</td>";
                 echo "<td>"."". "</td>";;
                 echo "<td>".$row['question_id']. "</td>";
                 echo "<td>".$row['quiz_id']. "</td>";
@@ -33,7 +29,7 @@ class QuizDisplayClass extends Connection{
 
                 echo "<td>".$row['topic_id']. "</td>";
                 echo "<td>"."". "</td>";
-
+                
 
                 echo "</tr>";
 
@@ -64,11 +60,18 @@ class QuizDisplayClass extends Connection{
         if($result->num_rows > 0){
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>";
-                echo "<td></td>";
+                echo "<td><a href='#' class='data_info_btn' data-id='".$row["quiz_id"]."' data-toggle='modal' data-target='#quiz_data_modal'><span class='glyphicon glyphicon-info-sign' style = 'padding-left: 10px;'></span></a>";
+
                 echo "<td>".$row['quiz_id']."</td>";
                 echo "<td>".$row['quiz_question']."</td>";
                 echo "<td>".$row['date_created']."</td>";
                 echo "<td>".$row['topic_id']."</td>";
+
+                echo "<td>";
+                echo "<a href='#' class='edit_quiz_btn' data-toggle='modal' data-target='#edit_quiz_modal' data-id='".$row["quiz_id"]."' style='margin-right:10px; color:blue;'><span class='glyphicon glyphicon-edit' ></span></a>";
+                
+                echo " <a href='#' class='archive_quiz_btn text-danger' data-toggle='modal' data-target='#archive_quiz_modal' data-id='".$row["quiz_id"]."'><span class='glyphicon glyphicon-trash'></span></a>";
+                echo "</td>";
                 echo "</tr>";
             }
         }
