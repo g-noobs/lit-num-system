8520bcc2649281d2cd51ab7819cfe09de7b086e8
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-28 23:41:13 +0800

    asd

--- PagesContent/UserContent/CommonUser/UploadExcelScript.php
+++ PagesContent/UserContent/CommonUser/UploadExcelScript.php
@@ -12,26 +12,31 @@ $(document).ready(function() {
             contentType: false,
             processData: false,
             success: function(response) {
-                var responseData = JSON.parse(response);
-                // Check if the form submission was successful
-                if (responseData.hasOwnProperty('success')) {
-                    $('#successAlert').text(responseData.success);
-                    $('#successBanner').show();
-                    setTimeout(function() {
-                        $("#successBanner").fadeOut("slow");
-                        location.reload();
-                    }, 1500);
-                    // You can redirect to a different page or perform other actions here
-                } else if (responseData.hasOwnProperty('error')) {
-                    $('#errorAlert').text(responseData.error);
-                    $('#errorBanner').show();
-                    setTimeout(function() {
-                        $("#errorBanner").fadeOut("slow");
-                        location.reload();
-                    }, 1500);
+                try {
+                    var responseData = JSON.parse(response);
+                    // Check if the form submission was successful
+                    if (responseData.hasOwnProperty('success')) {
+                        $('#successAlert').text(responseData.success);
+                        $('#successBanner').show();
+                        setTimeout(function() {
+                            $("#successBanner").fadeOut("slow");
+                            location.reload();
+                        }, 1500);
+                        // You can redirect to a different page or perform other actions here
+                    } else if (responseData.hasOwnProperty('error')) {
+                        $('#errorAlert').text(responseData.error);
+                        $('#errorBanner').show();
+                        setTimeout(function() {
+                            $("#errorBanner").fadeOut("slow");
+                            location.reload();
+                        }, 1500);
+                    }
                 }
-            }
-            // ,error: function() {
+                catch (e) {
+                    console.error('Error parsing JSON:', e);
+                }
+            },error: function(xhr, status, error) {
+                console.error('AJAX error:', status, error);
             //     $hideModal.modal('hide');
             //     //show alert banner id = errorBanner
             //     $('#errorAlert').text('An error occurred during the AJAX request.');
@@ -40,7 +45,7 @@ $(document).ready(function() {
             //         $("#errorBanner").fadeOut("slow");
             //         location.reload();
             //     }, 1500);
-            // }
+            }
         });
     });
 });
