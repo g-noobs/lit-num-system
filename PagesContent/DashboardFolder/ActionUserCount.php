4291612bb3164df431208aa989dd81c33d3e7e9f
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-10-30 05:42:10 +0000

    csdfs

--- PagesContent/DashboardFolder/ActionUserCount.php
+++ PagesContent/DashboardFolder/ActionUserCount.php
@@ -3,10 +3,10 @@ include_once "../../Database/ColumnCountClass.php";
 $count = new ColummCountClass();
 
 $table = "tbl_user_info";
-$col = "user_info_id";
-$adminCountQuery = "SELECT COUNT($col) as count FROM $table WHERE user_level_id = '0'";
-$teacherCountQuery = "SELECT COUNT($col) as count FROM $table WHERE user_level_id = '1'";
-$studentCountQuery = "SELECT COUNT($col) as count FROM $table WHERE user_level_id = '2'";
+
+$adminCountQuery = "SELECT COUNT(user_info_id) as count FROM $table WHERE user_level_id = '0';";
+$teacherCountQuery = "SELECT COUNT(user_info_id) as count FROM $table WHERE user_level_id = '1';";
+$studentCountQuery = "SELECT COUNT(user_info_id) as count FROM $table WHERE user_level_id = '2';";
 
 $adminCount = $count->columnCountNum($adminCountQuery);
 $teacherCount = $count->columnCountNum($teacherCountQuery);
