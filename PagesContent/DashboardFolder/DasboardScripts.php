f20d2b63ed481f731c3b1090730b492621484659
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-10-30 05:32:05 +0000

    asd

--- PagesContent/DashboardFolder/DasboardScripts.php
+++ PagesContent/DashboardFolder/DasboardScripts.php
@@ -6,36 +6,36 @@ $(function() {
     var student_count = "";
 
     $.ajax({
-        type: "GET",
-        url: "../PagesContent/DashboardFolder/ActionUserCount.php",
-        success: function(response) {
-            console.log(response); 
-            var responseData = JSON.parse(response);
-            admin_count = responseData.admin;
-            teacher_count = responseData.teacher;
-            student_count = responseData.student;
+    type: "GET",
+    url: "../PagesContent/DashboardFolder/ActionUserCount.php",
+    success: function (response) {
+        var responseData = JSON.parse(response);
+        admin_count = responseData.admin;
+        teacher_count = responseData.teacher;
+        student_count = responseData.student;
 
-            //DONUT CHART
-            var donut = new Morris.Donut({
-                element: 'user-chart',
-                resize: true,
-                colors: ["#3c8dbc", "#f56954", "#00a65a"],
-                data: [{
-                        label: "Admin",
-                        value: admin_count
-                    },
-                    {
-                        label: "Teacher",
-                        value: teacher_count
-                    },
-                    {
-                        label: "Learner",
-                        value: student_count
-                    }
-                ],
-                hideHover: 'auto'
-            });
-        }
-    });
+        // Create the Morris Donut Chart here
+        var donut = new Morris.Donut({
+            element: 'user-chart',
+            resize: true,
+            colors: ["#3c8dbc", "#f56954", "#00a65a"],
+            data: [
+                {
+                    label: "Admin",
+                    value: admin_count
+                },
+                {
+                    label: "Teacher",
+                    value: teacher_count
+                },
+                {
+                    label: "Learner",
+                    value: student_count
+                }
+            ],
+            hideHover: 'auto'
+        });
+    }
+});
 });
 </script>
