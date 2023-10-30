80450f3d0aa23aa52d50fbd1ae8aec54c9b77ddc
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-10-30 05:25:14 +0000

    asd

--- PagesContent/DashboardFolder/DasboardScripts.php
+++ PagesContent/DashboardFolder/DasboardScripts.php
@@ -8,33 +8,34 @@ $(function() {
     $.ajax({
         type: "GET",
         url: "../PagesContent/DashboardFolder/ActionUserCount.php",
-        success:function(response){
+        success: function(response) {
+            console.log(response); 
             var responseData = JSON.parse(response);
             admin_count = responseData.admin;
             teacher_count = responseData.teacher;
             student_count = responseData.student;
-        } 
-    });
 
-    //DONUT CHART
-    var donut = new Morris.Donut({
-        element: 'user-chart',
-        resize: true,
-        colors: ["#3c8dbc", "#f56954", "#00a65a"],
-        data: [{
-                label: "Admin",
-                value: admin_count
-            },
-            {
-                label: "Teacher",
-                value: teacher_count
-            },
-            {
-                label: "Learner",
-                value: student_count
-            }
-        ],
-        hideHover: 'auto'
+            //DONUT CHART
+            var donut = new Morris.Donut({
+                element: 'user-chart',
+                resize: true,
+                colors: ["#3c8dbc", "#f56954", "#00a65a"],
+                data: [{
+                        label: "Admin",
+                        value: admin_count
+                    },
+                    {
+                        label: "Teacher",
+                        value: teacher_count
+                    },
+                    {
+                        label: "Learner",
+                        value: student_count
+                    }
+                ],
+                hideHover: 'auto'
+            });
+        }
     });
 });
 </script>
