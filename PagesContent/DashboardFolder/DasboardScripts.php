c07babe2c73af325778bc894841b4864cf4c1383
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-10-30 05:52:40 +0000

    qwere

--- PagesContent/DashboardFolder/DasboardScripts.php
+++ PagesContent/DashboardFolder/DasboardScripts.php
@@ -2,44 +2,44 @@
 <script>
 $(function() {
     "use strict";
-    var admin_count = "2";
-    var teacher_count = "3";
-    var student_count = "4";
+    var admin_count = "";
+    var teacher_count = "";
+    var student_count = "";
 
-    var donut = new Morris.Donut({
-        element: 'user-chart',
-        resize: true,
-        colors: ["#3c8dbc", "#f56954", "#00a65a"],
-        data: [{
-                label: "Admin",
-                value: admin_count
-            },
-            {
-                label: "Teacher",
-                value: teacher_count
-            },
-            {
-                label: "Learner",
-                value: student_count
-            }
-        ],
-        hideHover: 'auto'
+
+
+    $.ajax({
+        type: "POST",
+        url: "../PagesContent/DashboardFolder/ActionUserCount.php",
+        success: function(response) {
+
+
+            var responseData = JSON.parse(response);
+            admin_count = responseData.admin;
+            teacher_count = responseData.teacher;
+            student_count = responseData.student;
+
+            var donut = new Morris.Donut({
+                element: 'user-chart',
+                resize: true,
+                colors: ["#3c8dbc", "#f56954", "#00a65a"],
+                data: [{
+                        label: "Admin",
+                        value: admin_count
+                    },
+                    {
+                        label: "Teacher",
+                        value: teacher_count
+                    },
+                    {
+                        label: "Learner",
+                        value: student_count
+                    }
+                ],
+                hideHover: 'auto'
+            });
+
+        }
     });
-
-    // $.ajax({
-    //     type: "GET",
-    //     url: "../PagesContent/DashboardFolder/ActionUserCount.php",
-    //     success: function(response) {
-
-
-    //         var responseData = JSON.parse(response);
-    //         admin_count = responseData.admin;
-    //         teacher_count = responseData.teacher;
-    //         student_count = responseData.student;
-
-    //         // Create the Morris Donut Chart here
-
-    //     }
-    // });
 });
 </script>
