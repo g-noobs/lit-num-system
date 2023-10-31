2b24ad5ac942cc04c4de521887c0b3bb06a73440
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-31 21:03:26 +0800

    modal-body  end tag

--- PagesContent/UserContent/UserModalFolder/AddLearnerModal.php
+++ PagesContent/UserContent/UserModalFolder/AddLearnerModal.php
@@ -114,6 +114,7 @@
                             </div>
                         </div>
                     </div>
+                </div>
             </form>
         </div>
         <!-- /.modal-content -->
