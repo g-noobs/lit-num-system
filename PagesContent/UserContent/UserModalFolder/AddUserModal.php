9d7b2a5e798314e98e6aa5a0e2ba37174a1cf464
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-10-31 01:00:45 +0000

    sdf

--- PagesContent/UserContent/UserModalFolder/AddUserModal.php
+++ PagesContent/UserContent/UserModalFolder/AddUserModal.php
@@ -11,7 +11,7 @@
                 <div class="modal-body">
                     <div class="box-body box-warning">
                         <div class="row">
-                            <div class="col-sm-2">
+                            <div class="col-sm-5">
                                 <div class="form-group">
                                     <label for="last_name">Enter Last Name:</label>
                                     <input type="text" name="last_name" class="form-control" placeholder="Last Name"
@@ -25,7 +25,7 @@
                                         required autocomplete="given-name" />
                                 </div>
                             </div>
-                            <div class="col-sm-5">
+                            <div class="col-sm-2">
                                 <div class="form-group">
                                     <label for="middle_initial">Middle Initial
                                         <span><small>*optional</small></span></label>
@@ -36,7 +36,7 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-sm-2">
+                            <div class="col-sm-4">
                                 <div class="form-group">
                                     <label for="gender">Select Gender:</label>
                                     <select class="form-control" name="gender" placeholder="Gender" required>
