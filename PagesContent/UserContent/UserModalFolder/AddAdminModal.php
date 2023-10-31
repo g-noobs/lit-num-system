f91c7b3a48e7dadc93688131819a7a38fe70e731
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-31 20:36:14 +0800

    asd

--- PagesContent/UserContent/UserModalFolder/AddAdminModal.php
+++ PagesContent/UserContent/UserModalFolder/AddAdminModal.php
@@ -28,7 +28,7 @@
                             <div class="col-sm-4">
                                 <div class="form-group">
                                     <label for="user_middle_initial">Middle Initial
-                                        <p class="help-block">*optional</p></label>
+                                        <span><small><p>*optional</p><</small></span></label>
                                     <select name="user_middle_initial" id="user_middle_initial" class="form-control input-sm">
                                         <option value="">-</option>
                                     </select>
