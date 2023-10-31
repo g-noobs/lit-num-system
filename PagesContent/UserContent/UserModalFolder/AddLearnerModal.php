d0e74d661979910d8b3f88a33ce0691c28f110cd
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-31 20:59:30 +0800

    asd

--- PagesContent/UserContent/UserModalFolder/AddLearnerModal.php
+++ PagesContent/UserContent/UserModalFolder/AddLearnerModal.php
@@ -14,21 +14,22 @@
                             <div class="col-sm-5">
                                 <div class="form-group">
                                     <label for="last_name">Enter Last Name:</label>
-                                    <input type="text" name="last_name" class="form-control input-sm" placeholder="Last Name"
-                                        required>
+                                    <input type="text" name="last_name" class="form-control input-sm"
+                                        placeholder="Last Name" required>
                                 </div>
                             </div>
                             <div class="col-sm-5">
                                 <div class="form-group">
                                     <label for="first_name">Enter First Name:</label>
-                                    <input type="text" name="first_name" class="form-control input-sm" placeholder="First Name"
-                                        required autocomplete="given-name" />
+                                    <input type="text" name="first_name" class="form-control input-sm"
+                                        placeholder="First Name" required autocomplete="given-name" />
                                 </div>
                             </div>
                             <div class="col-sm-2">
                                 <div class="form-group">
                                     <label for="user_middle_initial">Middle Initial</label>
-                                    <select name="user_middle_initial" id="user_middle_initial" class="form-control input-sm">
+                                    <select name="user_middle_initial" id="user_middle_initial"
+                                        class="form-control input-sm">
                                         <option value="">-</option>
                                     </select>
                                 </div>
@@ -56,11 +57,46 @@
                             <div class="col-sm-4">
                                 <div class="form-group">
                                     <label for="email">Enter Email Address:</label>
-                                    <input type="email" name="email" class="form-control input-sm" placeholder="Email" required
-                                        autocomplete="off" />
+                                    <input type="email" name="email" class="form-control input-sm" placeholder="Email"
+                                        required autocomplete="off" />
                                 </div>
                             </div>
                         </div>
+                        <div class="row">
+                            <label for="Address">Address</label>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div clas="form-group">
+                                    <label for="street_address">Street</label>
+                                    <input type="text" name="street_address" class="form-control input-sm"
+                                        placeholder="Street" required autocomplete="street-address" />
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label for="Barangay">Baranggay</label>
+                                    <input type="text" name="barangay_address" class="form-control input-sm" 
+                                    placeholder="Barangay"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <label for="city">City</label>
+                                <input type="text" name="city_address" class="form-control input-sm" placeholder="City"
+                                    required autocomplete="address-level2" />
+                            </div>
+                            <div class="col-sm-4">
+                                <label for="province">Province</label>
+                                <input type="text" name="province_address" class="form-control input-sm"
+                                    placeholder="Province" required autocomplete="address-level1" />
+                            </div>
+                            <div class="col-sm-4">
+                                <label for="zip_code">Zip Code</label>
+                                <input type="text" name="zip_code_address" class="form-control input-sm"
+                                    placeholder="Zip Code" required autocomplete="postal-code" />
+                            </div>
                     </div>
                     <!-- /.box-body -->
                     <div class="modal-footer">
