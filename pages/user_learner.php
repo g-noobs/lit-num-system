43ecaadd3fc3601309a99f52a595ebcdcdb9409d
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-04 12:47:47 +0800

    asd

--- pages/user_learner.php
+++ pages/user_learner.php
@@ -93,8 +93,8 @@
                     </div>
                     <div id="response"></div>
                     <div class="form-group">
-                        <input type="submit" name="upload" id="upload" value="Upload" class="btn btn-info" />
-                        <button type="reset" class="btn btn-default pull-left">Clear</button>
+                        <input type="submit" name="upload" id="upload" value="Upload" class="btn btn-info pull-left" />
+                        <button type="reset" class="btn btn-default">Clear</button>
                     </div>
                 </form>
             </section>
