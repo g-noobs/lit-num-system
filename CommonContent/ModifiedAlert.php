06c9afcfa1234e9f836e8c40e388316e375e9fac
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-04 21:05:14 +0800

    asd

--- CommonContent/ModifiedAlert.php
+++ CommonContent/ModifiedAlert.php
@@ -14,6 +14,20 @@
     z-index: 1000;
     /* Set a high z-index to ensure it's on top of other elements */
 }
+.errorBanner, .successBanner{
+    display: none;
+    opacity: 0.75;
+    position: fixed;
+    /* Make it a fixed position */
+    top: 1;
+    /* Align to the top of the viewport */
+    right: 0;
+    /* Align to the left of the viewport */
+    width: 65%;
+    /* Full width */
+    z-index: 1000;
+    /* Set a high z-index to ensure it's on top of other elements */
+}
 </style>
 
 <div class="alert alert-danger alert-dismissible fade in" id="errorBanner">
