07943ba4c400f397e634144e01838e322338b83b
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-05 19:09:51 +0800

    asd

--- PagesContent/ClassContent/ClassModal/AddArchiveEditModal.php
+++ PagesContent/ClassContent/ClassModal/AddArchiveEditModal.php
@@ -1,3 +1,21 @@
+<!-- no data checked modal -->
+<div class="modal fade" id="no_data_selected_modal" role="dialog">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title"></h4>
+            </div>
+            <div class="modal-body">
+                <h3>No data selected.</h3>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="modal fade" id="add-class">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -73,25 +91,3 @@
     <!-- /.modal-dialog -->
 </div>
 <!-- /.modal Edit Class -->
-
-
-
-
-<!-- no data checked modal -->
-<!-- Modal -->
-<div class="modal fade" id="no_data_selected_modal" role="dialog">
-    <div class="modal-dialog modal-sm">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title"></h4>
-            </div>
-            <div class="modal-body">
-                <h3>No data selected.</h3>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
