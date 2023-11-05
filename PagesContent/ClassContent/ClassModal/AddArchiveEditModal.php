20282d30a38563df07d965df3142f4da339d977d
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-05 19:06:22 +0800

    asdf

--- PagesContent/ClassContent/ClassModal/AddArchiveEditModal.php
+++ PagesContent/ClassContent/ClassModal/AddArchiveEditModal.php
@@ -55,7 +55,6 @@
                             <label for="class_id">Class ID:</label>
                             <input type="hidden" name="class_id">
                             <input type="text" readonly id="user" name="class_id" class="form-control">
-
                         </div>
                         <div class="form-group">
                             <label for="class_name">Edit Class Name:</label>
@@ -77,27 +76,7 @@
 <!-- /.modal Edit Class -->
 
 
-<div class="modal fade" id="archive-class">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Are you sure you wanted to ARCHIVE this user?</h4>
-                <form role="form" action="../PagesContent/ClassContent/ActionFolder/ArchiveClassAction.php"
-                    method="post">
-                    <div class="form-group">
-                        <input type="text" readonly name="class_id" class="form-control">
-                        <button type="submit" class="btn btn-danger pull-left">Archive</button>
-                        <button type="reset" class="btn btn-default pull-left" data-dismiss="modal">Cancel</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
+
 
 <!-- no data checked modal -->
 <!-- Modal -->
