40e27f8e15ce4d6d820504ac3f9492590f206be3
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-11-06 06:02:05 +0000

    asd

---
+++ PagesContent/UserContent/AllUserScript/AssignModuleScript.php
@@ -0,0 +1,29 @@
+
+
+<script>
+$(document).ready(function() {
+    // Initialize a counter to keep track of the number of added selects
+    var selectCounter = 1;
+
+    // When the "Assign More Module" button is clicked
+    $('#assign_more_module').on('click', function(e) {
+        e.preventDefault();
+        
+        // Clone the original select element
+        var originalSelect = $('.assign_module').first();
+        var clonedSelect = originalSelect.clone();
+
+        // Modify the cloned select's name and ID to make them unique
+        var newSelectName = 'assign_module_id_' + selectCounter;
+        var newSelectId = 'assign_module_id_' + selectCounter;
+        clonedSelect.attr('name', newSelectName);
+        clonedSelect.attr('id', newSelectId);
+
+        // Append the cloned select to the form
+        $('.assign_module_form').append(clonedSelect);
+
+        // Increment the counter
+        selectCounter++;
+    });
+});
+</script>
