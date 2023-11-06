f3fd322a25405cc16540a509611e67853b286293
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-11-06 04:03:02 +0000

    asd

--- PagesContent/UserContent/UserModalFolder/AssignClassModal.php
+++ PagesContent/UserContent/UserModalFolder/AssignClassModal.php
@@ -12,19 +12,15 @@
                 <div class="modal-body">
                     <div class="box-body box-warning">
                         <div class="form-group row">
-                            <div class="col-xs-2"></div>
-                            <div class="col-xs-8">
-                                <label for="assign_class_id">Choose a Class to Assign</label>
-                                <select name="assign_class_id" class="form-control input-xs assign_class">
-                                    <?php include_once "../Database/DisplayAssignClass.php";
+                            <label for="assign_class_id">Choose a Class to Assign</label>
+                            <select name="assign_class_id" class="form-control input-xs assign_class">
+                                <?php include_once "../Database/DisplayAssignClass.php";
                                         $classlist = new DisplayAssignClass();
                                         $classlist->displayClassList();
                                     ?>
-                                </select>
-                                <a href="#" id="assign_more_class" type="button" data-toggle="tooltip" title="Add More Class"
-                                    class="text-success"><i class="fa fa-plus"></i></a>
-                            </div>
-                            <div class="col-xs-2"></div>
+                            </select>
+                            <a href="#" id="assign_more_class" type="button" data-toggle="tooltip"
+                                title="Add More Class" class="text-success"><i class="fa fa-plus"></i></a>
                         </div>
                     </div>
                     <!-- /.box-body -->
