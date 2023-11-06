6da62c373e5bed484b58d1e89129475bbd825901
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-11-06 05:59:32 +0000

    asd

--- PagesContent/UserContent/AllUserScript/AssignTeacherScript.php
+++ PagesContent/UserContent/AllUserScript/AssignTeacherScript.php
@@ -1,13 +1,28 @@
+
 <script>
 $(document).ready(function() {
-    // Handle the click event for the "Assign More Class" button
-    $("#assign_more_class_btn").click(function(e) {
-        e.preventDefault();
+    // Initialize a counter to keep track of the number of added selects
+    var selectCounter = 1;
 
-        // Clone the select element and append it to the form
-        var originalSelect = $(".assign_c select");
-        var newSelect = originalSelect.clone();
-        $(".form-group.row").append(newSelect);
+    // When the "Assign More Class" button is clicked
+    $('#assign_more_class_btn').on('click', function(e) {
+        e.preventDefault();
+        
+        // Clone the original select element
+        var originalSelect = $('.assign_class').first();
+        var clonedSelect = originalSelect.clone();
+
+        // Modify the cloned select's name and ID to make them unique
+        var newSelectName = 'assign_class_id_' + selectCounter;
+        var newSelectId = 'assign_class_id_' + selectCounter;
+        clonedSelect.attr('name', newSelectName);
+        clonedSelect.attr('id', newSelectId);
+
+        // Append the cloned select to the form
+        $('.assign_c').append(clonedSelect);
+
+        // Increment the counter
+        selectCounter++;
     });
 });
 </script>
