0f8dca97579d8a7fe01f692cfdd5d3a2e28412e9
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-11-06 04:31:21 +0000

    asf

--- PagesContent/UserContent/AllUserScript/AssignTeacherScript.php
+++ PagesContent/UserContent/AllUserScript/AssignTeacherScript.php
@@ -1,40 +1,37 @@
-<!-- Add this script tag to include jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
 <script>
 $(document).ready(function() {
-    // Counter to keep track of the number of added selects
-    var selectCounter = 1;
-
-    // Handle the "assign_more_class_btn" button click event
-    $("#assign_more_class_btn").on("click", function(e) {
+    // Function to handle adding additional select elements
+    $("#assign_more_class_btn").click(function(e) {
         e.preventDefault();
 
         // Clone the original select element
-        var $originalSelect = $(".assign_class:first");
-        var $newSelect = $originalSelect.clone();
+        var originalSelect = $(".assign_class").eq(0);
+        var clonedSelect = originalSelect.clone();
 
-        // Update the ID and name attributes to ensure uniqueness
-        var newSelectId = "assign_class_" + selectCounter;
-        var newSelectName = "assign_class_id_" + selectCounter;
-        $newSelect.attr("id", newSelectId);
-        $newSelect.attr("name", newSelectName);
+        // Remove the selected option from the cloned select element
+        clonedSelect.find("option:selected").prop("disabled", true);
 
-        // Append the new select to the form
-        $(".modal-body .form-group:last").before($newSelect);
-
-        selectCounter++;
-
-        // Disable or remove selected options in other select elements
-        $(".assign_class").not($newSelect).find("option:selected").prop("disabled", true);
+        // Append the cloned select element to the form
+        $(".modal-body .form-group.row").append(clonedSelect);
     });
 
-    // Handle change event for select elements to re-enable selected options
-    $(".assign_class").on("change", function() {
-        var selectedValue = $(this).val();
-        $(".assign_class").not(this).find("option").prop("disabled", false);
-        $(".assign_class").not(this).each(function() {
-            $(this).find('option[value="' + selectedValue + '"]').prop("disabled", true);
+    // Function to handle form submission
+    $("#assign_class_form").submit(function(e) {
+        e.preventDefault();
+
+        // Add your form submission logic here
+
+        // For example, you can use AJAX to submit the form data
+        $.ajax({
+            url: "your_server_endpoint",
+            type: "POST",
+            data: $(this).serialize(),
+            success: function(response) {
+                // Handle success response
+            },
+            error: function(error) {
+                // Handle error response
+            }
         });
     });
 });
