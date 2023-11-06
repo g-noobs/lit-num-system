f9a2441c9c489e3f398ab055554e180cb4b1ed7d
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-11-06 05:37:43 +0000

    asd

--- PagesContent/UserContent/AllUserScript/AssignTeacherScript.php
+++ PagesContent/UserContent/AllUserScript/AssignTeacherScript.php
@@ -1,37 +1,35 @@
-<!-- Add the following script in your HTML file -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
 <script>
 $(document).ready(function() {
-    // Initialize a counter to keep track of added selects
-    var selectCounter = 1;
+    var $assignClassForm = $('#assign_class_form');
+    var $assignClassSelect = $assignClassForm.find('.assign_class');
+    var $assignMoreClassBtn = $('#assign_more_class_btn');
     
-    // Clone the initial select and reset its selected option
-    function createNewSelect() {
-        var newSelect = $(".assign_class:first").clone();
-        newSelect.find("option:selected").prop("selected", false);
-        newSelect.find("option:first").prop("selected", true);
-        newSelect.attr("name", "assign_class_id_" + selectCounter);
-        selectCounter++;
-        return newSelect;
-    }
-    
-    // Add a new select when the "Assign More Class" button is clicked
-    $("#assign_more_class_btn").click(function() {
-        var newSelect = createNewSelect();
-        $(".form-group:last").append(newSelect);
-        
-        // Disable the button if all options are selected
-        if ($(".assign_class option:not(:selected)").length === 0) {
-            $("#assign_more_class_btn").prop("disabled", true);
-        }
-    });
+    // Disable the "Assign Teacher" button initially
+    $assignClassForm.find('button[type="submit"]').prop('disabled', true);
 
-    // Handle disabling or removing options from other selects
-    $(".assign_class").change(function() {
-        var selectedValue = $(this).val();
-        $(".assign_class").not(this).find("option").prop("disabled", false);
-        $(".assign_class").not(this).find("option[value='" + selectedValue + "']").prop("disabled", true);
+    // Add an additional select when the "Assign More Class" button is clicked
+    $assignMoreClassBtn.on('click', function() {
+        var $newSelect = $assignClassSelect.clone();
+        var $removeButton = $('<a href="#" class="remove-class-btn" data-toggle="tooltip" title="Remove Class"><i class="fa fa-times text-danger"></i></a>');
+        
+        // Add a new select and remove button
+        $newSelect.appendTo($assignClassForm.find('.form-group.row'));
+        $removeButton.appendTo($newSelect.parent());
+        
+        // Enable the "Assign Teacher" button if at least one select is added
+        $assignClassForm.find('button[type="submit"]').prop('disabled', false);
+        
+        // Add a click event to the remove button
+        $removeButton.on('click', function(e) {
+            e.preventDefault();
+            $newSelect.remove();
+            $removeButton.remove();
+            
+            // Check if there are any selects left, and disable the button if none
+            if ($assignClassForm.find('.assign_class').length === 1) {
+                $assignClassForm.find('button[type="submit"]').prop('disabled', true);
+            }
+        });
     });
 });
 </script>
