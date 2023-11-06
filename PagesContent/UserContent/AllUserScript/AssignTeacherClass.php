e3051eadd8ad64d5f40ce23ed5943b68431d7673
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-11-06 03:52:33 +0000

    addg

--- PagesContent/UserContent/AllUserScript/AssignTeacherClass.php
+++ PagesContent/UserContent/AllUserScript/AssignTeacherClass.php
@@ -1,9 +1,30 @@
 <script>
 $(document).ready(function() {
     $('#assign_more_class').click(function() {
-        var $select = $('select.assign_class').first().clone();
-        $select.find('option:selected').prop('disabled', true); // Disable selected options
-        $('select.assign_class').last().after($select);
+        var $assignClassSelect = $(
+            '<select name="assign_class_id" class="form-control input-xs assign_class"></select>');
+        var $assignClassOption = $('.assign_class option').clone();
+
+        $assignClassSelect.append($assignClassOption);
+
+        // Disable the selected option in other selects
+        $('select.assign_class').each(function() {
+            var selectedOptionValue = $(this).val();
+            if (selectedOptionValue) {
+                $assignClassSelect.find('option[value="' + selectedOptionValue + '"]').prop(
+                    'disabled', true);
+            }
+        });
+
+        // Append the new select to the modal body
+        $('.modal-body').find('.form-group.row').append($('<div class="col-xs-8"></div>').append(
+            $assignClassSelect));
+    });
+
+    // Handle form submission
+    $('#assign_class_form').on('submit', function(e) {
+        e.preventDefault();
+        // Handle the form submission logic here
     });
 });
 </script>
