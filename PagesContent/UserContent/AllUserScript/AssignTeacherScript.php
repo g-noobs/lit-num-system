48c4cef86b7c1ea3af83e724645502aab04f7d68
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-11-06 05:55:30 +0000

    asddd

--- PagesContent/UserContent/AllUserScript/AssignTeacherScript.php
+++ PagesContent/UserContent/AllUserScript/AssignTeacherScript.php
@@ -1,23 +1,13 @@
 <script>
-$(document).ready(function () {
-    $("#assign_more_class_btn").click(function (e) {
+$(document).ready(function() {
+    // Handle the click event for the "Assign More Class" button
+    $("#assign_more_class_btn").click(function(e) {
         e.preventDefault();
-        var select = $(".assign_class").clone();
 
-        // Add a remove button to the new select element
-        var removeButton = $("<a href='#' class='remove-class'><i class='fa fa-times'></i></a>");
-        removeButton.click(function (e) {
-            e.preventDefault();
-            $(this).closest('.form-group').remove();
-        });
-        select.after(removeButton);
-
-        // Append the new select element to the container
-        $(".assigned-classes").append(select);
-
-        // Reset the selected option in the original select
-        $(".assign_class").val($(".assign_class option:first").val());
+        // Clone the select element and append it to the form
+        var originalSelect = $(".form-group.row select");
+        var newSelect = originalSelect.clone();
+        $(".form-group.row").append(newSelect);
     });
 });
-
 </script>
