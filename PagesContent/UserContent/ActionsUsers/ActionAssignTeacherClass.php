d1fda94780c1bb1aadb587585e72aace1fb95a9c
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-07 18:18:56 +0800

    asd

--- PagesContent/UserContent/ActionsUsers/ActionAssignTeacherClass.php
+++ PagesContent/UserContent/ActionsUsers/ActionAssignTeacherClass.php
@@ -20,7 +20,7 @@ $response = array();
     // For example, you can loop through the $assign_class_id array and perform actions with it
     foreach ($assign_class_id as $class_id) {
         // Check if the combination of user_info_id and class_id exists
-    $sql = "SELECT COUNT(*) AS count FROM $table WHERE user_info_id = '$user_teacher_id' AND class_id = '$class_id';";
+    $sql = "SELECT COUNT(class_assign_teacher_id) AS count FROM $table WHERE user_info_id = '$user_teacher_id' AND class_id = '$class_id';";
     $result = $conn->getConnection()->query($sql);
         if ($result) {
             $row = $result->fetch_row();
