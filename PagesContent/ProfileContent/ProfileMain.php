bd99d425c20327d33838801d69599e80830a7034
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-08 20:12:20 +0800

    asd

--- PagesContent/ProfileContent/ProfileMain.php
+++ PagesContent/ProfileContent/ProfileMain.php
@@ -14,8 +14,8 @@
             </div>
         </div>
         <div class="box-body">
-            <a href='#' id="edit-icon" type="button" class="btn btn-primary"><span
-                    class='glyphicon glyphicon-edit'></span>Edit</a>
+            <a href='#' id="edit-icon" type="button" class="btn btn-primary">Edit <span
+                    class='glyphicon glyphicon-edit'></span></a>
 
             <form id="edit_profile_form" method="post">
 
