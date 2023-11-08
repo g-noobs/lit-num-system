92cd36b1d4f619e7cc1c9be613ecd75e1d5a15a8
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-08 21:12:21 +0800

    asd

--- PagesContent/ProfileContent/ProfileMain.php
+++ PagesContent/ProfileContent/ProfileMain.php
@@ -24,14 +24,14 @@
                             <div class="col-sm-5">
                                 <div class="form-group">
                                     <label for="last_name">Enter Last Name:</label>
-                                    <input type="text" name="last_name" class="form-control input-sm"
+                                    <input type="text" name="last_name" class="form-control"
                                         placeholder="Last Name" required>
                                 </div>
                             </div>
                             <div class="col-sm-5">
                                 <div class="form-group">
                                     <label for="first_name">Enter First Name:</label>
-                                    <input type="text" name="first_name" class="form-control input-sm"
+                                    <input type="text" name="first_name" class="form-control"
                                         placeholder="First Name" required autocomplete="given-name" />
                                 </div>
                             </div>
@@ -39,7 +39,7 @@
                                 <div class="form-group">
                                     <label for="user_middle_initial">Middle Initial</label>
                                     <select name="user_middle_initial" id="user_middle_initial"
-                                        class="form-control input-sm">
+                                        class="form-control ">
                                         <option value="">-</option>
                                     </select>
                                 </div>
@@ -49,7 +49,7 @@
                             <div class="col-sm-4">
                                 <div class="form-group">
                                     <label for="gender">Select Gender:</label>
-                                    <select class="form-control input-sm" name="gender" placeholder="Gender" required>
+                                    <select class="form-control" name="gender" placeholder="Gender" required>
                                         <option value="Male">Male</option>
                                         <option value="Female">Female</option>
                                         <option value="None">None</option>
@@ -59,14 +59,14 @@
                             <div class="col-sm-4">
                                 <div class="form-group">
                                     <label for="phone_num">Phone:</label>
-                                    <input type="text" class="form-control input-sm" name="phone_num" id="phone_num"
+                                    <input type="text" class="form-control" name="phone_num" id="phone_num"
                                         required>
                                 </div>
                             </div>
                             <div class="col-sm-4">
                                 <div class="form-group">
                                     <label for="email">Enter Email Address:</label>
-                                    <input type="email" name="email" class="form-control input-sm" placeholder="Email"
+                                    <input type="email" name="email" class="form-control" placeholder="Email"
                                         required autocomplete="off" />
                                 </div>
                             </div>
@@ -86,7 +86,7 @@
                             <div class="col-sm-6 form-group">
                                 <div class="input-group">
                                     <input type="password" name="password" id="password" required
-                                        class="form-control input-sm" placeholder="Password">
+                                        class="form-control" placeholder="Password">
                                     <span class="input-group-btn">
                                         <button class="btn btn-default" type="button" id="togglePassword">
                                             <span class="glyphicon glyphicon-eye-open" id="password-icon"></span>
@@ -99,7 +99,7 @@
                             <div class="col-sm-6 form-group">
                                 <div class="input-group">
                                     <input type="password" name="confirmPassword" id="confirmPassword" required
-                                        class="form-control input-sm" placeholder="Confirm Password">
+                                        class="form-control" placeholder="Confirm Password">
                                     <span class="input-group-btn">
                                         <button class="btn btn-default" type="button" id="toggleConfirmPassword">
                                             <span class="glyphicon glyphicon-eye-open"
