287595900acf7f9f75eeb4c4f344985e4d7bc2fb
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-08 20:27:09 +0800

    asd

--- PagesContent/ProfileContent/ProfileMain.php
+++ PagesContent/ProfileContent/ProfileMain.php
@@ -69,35 +69,37 @@
                                     <input type="email" name="email" class="form-control input-sm" placeholder="Email"
                                         required autocomplete="off" />
                                 </div>
+                                <div class="form-group" data-select2-id="41">
+                                    <label>Multiple</label>
+                                    <select class="form-control select2 select2-hidden-accessible" multiple=""
+                                        data-placeholder="Select a State" style="width: 100%;" data-select2-id="7"
+                                        tabindex="-1" aria-hidden="true">
+                                        <option data-select2-id="42">Alabama</option>
+                                        <option data-select2-id="43">Alaska</option>
+                                        <option data-select2-id="44">California</option>
+                                        <option data-select2-id="45">Delaware</option>
+                                        <option data-select2-id="46">Tennessee</option>
+                                        <option data-select2-id="47">Texas</option>
+                                        <option data-select2-id="48">Washington</option>
+                                    </select><span
+                                        class="select2 select2-container select2-container--default select2-container--below"
+                                        dir="ltr" data-select2-id="8" style="width: 100%;"><span class="selection"><span
+                                                class="select2-selection select2-selection--multiple" role="combobox"
+                                                aria-haspopup="true" aria-expanded="false" tabindex="-1">
+                                                <ul class="select2-selection__rendered">
+                                                    <li class="select2-search select2-search--inline"><input
+                                                            class="select2-search__field" type="search" tabindex="0"
+                                                            autocomplete="off" autocorrect="off" autocapitalize="none"
+                                                            spellcheck="false" role="textbox" aria-autocomplete="list"
+                                                            placeholder="Select a State" style="width: 469.6px;">
+                                                    </li>
+                                                </ul>
+                                            </span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="form-group" data-select2-id="41">
-                        <label>Multiple</label>
-                        <select class="form-control select2 select2-hidden-accessible" multiple=""
-                            data-placeholder="Select a State" style="width: 100%;" data-select2-id="7" tabindex="-1"
-                            aria-hidden="true">
-                            <option data-select2-id="42">Alabama</option>
-                            <option data-select2-id="43">Alaska</option>
-                            <option data-select2-id="44">California</option>
-                            <option data-select2-id="45">Delaware</option>
-                            <option data-select2-id="46">Tennessee</option>
-                            <option data-select2-id="47">Texas</option>
-                            <option data-select2-id="48">Washington</option>
-                        </select><span
-                            class="select2 select2-container select2-container--default select2-container--below"
-                            dir="ltr" data-select2-id="8" style="width: 100%;"><span class="selection"><span
-                                    class="select2-selection select2-selection--multiple" role="combobox"
-                                    aria-haspopup="true" aria-expanded="false" tabindex="-1">
-                                    <ul class="select2-selection__rendered">
-                                        <li class="select2-search select2-search--inline"><input
-                                                class="select2-search__field" type="search" tabindex="0"
-                                                autocomplete="off" autocorrect="off" autocapitalize="none"
-                                                spellcheck="false" role="textbox" aria-autocomplete="list"
-                                                placeholder="Select a State" style="width: 469.6px;"></li>
-                                    </ul>
-                                </span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
-                    </div>
+
                     <!-- /.box-body -->
                     <div class="modal-footer">
                         <div class="form-group">
