f37766b150ef2d8fee24bc7718f34b0b8bf1ded3
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-11-13 01:38:03 +0000

    asd

--- PagesContent/ProfileContent/JqueryProfile.php
+++ PagesContent/ProfileContent/JqueryProfile.php
@@ -4,8 +4,8 @@ $(document).ready(function() {
 
     // Hide the update button initially
     $('#update-btn').hide();
-    $('input, select').prop('readonly', true).prop('disabled', false);
-
+    $('input').prop('readonly', true).prop('disabled', true);
+    $('select').prop('readonly', true).prop('disabled', false);
     // Function to toggle the edit mode
     function toggleEditMode() {
         if (editMode) {
