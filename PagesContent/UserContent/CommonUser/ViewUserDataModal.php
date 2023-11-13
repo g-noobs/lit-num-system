85e1af30ac0832b7dd74f6c86dd9e6aa3edc0f40
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-13 04:40:00 +0000

    asd

--- PagesContent/UserContent/CommonUser/ViewUserDataModal.php
+++ PagesContent/UserContent/CommonUser/ViewUserDataModal.php
@@ -5,23 +5,39 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
-                <h4 class="modal-title">Data for User: 
+                <h4 class="modal-title">Data for User:
                     <span id="user_id_data"></span>
                 </h4>
             </div>
             <!-- /.modal-header -->
             <div class=" modal-body">
-                <div class="box box-widget widget-user-2">
-                    <div class="box-footer no-padding">
-                        <ul class="nav nav-stacked">
-                            <li><strong>Personal ID: </strong><span id="personal_id_data"></span></li>
-                            <li><strong>Full Name: </strong><span id="full_name_data"></span> </li>
-                            <li><strong>Gender: </strong><span id="gender_data"></span></li>
-                            <li><strong>Email: </strong><span id="email_data"></span></li>
-                            <li><strong>Address: </strong> <span id="Address"></span></li>
-                            <li><strong>Username: </strong><span id="username_data"></span></li>
-                            <li><strong>Password: </strong><span id="password_data"></span></li>
-                        </ul>
+                <div class="box box-solid">
+                    <div class="box-body">
+                        <dl class="dl-horizontal">
+                            <dt>Personal ID: </dt>
+                            <dd id="personal_id_data"></dd>
+
+                            <dt>Full Name: </dt>
+                            <dd id="full_name_data"></dd>
+
+                            <dt>Gender: </dt>
+                            <dd id="gender_data"></dd>
+
+                            <dt>Email: </dt>
+                            <dd id="email_data"></dd>
+
+                            <dt>Phone#: </dt>
+                            <dd id="phone_num_data"></dd>
+
+                            <dt>Addres:</dt>
+                            <dd id="full_address_data"></dd>
+
+                            <dt>Username:</dt>
+                            <dd id="username_data"></dd>
+
+                            <dt>Password</dt>
+                            <dd id="password_data"></dd>
+                        </dl>
                     </div>
                 </div>
                 <!-- /.box-body -->
