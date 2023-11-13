a4bf9306ebb75c522e9fc995b10d62f25a9f4dd8
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-13 05:35:54 +0000

    zxc

--- PagesContent/SubjectFolder/EssentialsSubj/ModalSubj.php
+++ PagesContent/SubjectFolder/EssentialsSubj/ModalSubj.php
@@ -5,17 +5,17 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
-                <h4 class="modal-title" id="modal-title">Enter Subject Information</h4>
+                <h4 class="modal-title" id="modal-title">Enter Module Information</h4>
             </div>
             <form id="subjForm">
                 <div class="modal-body">
                     <div class="box-body">
                         <div class="form-group">
-                            <label for="subj_name_add">Enter Subject Name:</label>
+                            <label for="subj_name_add">Enter Module Name:</label>
                             <input type="text" name="subj_name_add" class="form-control" placeholder="Subject Name" required>
                         </div>
                         <div class="form-group">
-                            <label for="subj_add_desc">Subject Description:</label>
+                            <label for="subj_add_desc">Module Description:</label>
                             <input type="text" name="subj_add_desc" class="form-control" placeholder="Description" required>
                         </div>
                     </div>
@@ -23,7 +23,7 @@
                 <!-- /.box-body -->
                 <div class="modal-footer">
                     <div class="form-group">
-                        <button type="submit" class="btn btn-warning pull-left">Add Subj</button>
+                        <button type="submit" class="btn btn-warning pull-left">Add Module</button>
                         <button type="reset" class="btn btn-default pull-right" data-dismiss="modal">Cancel</button>
                     </div>
                 </div>
