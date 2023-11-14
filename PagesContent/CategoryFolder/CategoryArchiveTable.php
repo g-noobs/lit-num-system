fdd6e57103350db1e6a29e829a2791f965819b6b
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-14 05:40:53 +0000

    asd

--- PagesContent/CategoryFolder/CategoryArchiveTable.php
+++ PagesContent/CategoryFolder/CategoryArchiveTable.php
@@ -55,8 +55,20 @@
 <!-- dropdown query -->
 <?php include_once "CategoryCommon/CategoryJquery.php";?>
 
-<!-- input search -->
-<?php include_once "../../CommonContent/CommonAllScript.php"?>
-
 <!-- activate script -->
 <?php include_once "CategoryScript/ActivateScript.php";?>
+
+
+<!-- Jquery for Search -->
+<script>
+$(document).ready(function() {
+    $("#userInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("tbody tr").filter(function() {
+            var rowText = $(this).text().toLowerCase();
+            var pText = $(this).find("p").text().toLowerCase();
+            $(this).toggle(rowText.indexOf(value) > -1 || pText.indexOf(value) > -1);
+        });
+    });
+});
+</script>
