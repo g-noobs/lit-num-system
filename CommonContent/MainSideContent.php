79836795eb44d8ce6ef79e0c85ae8df7f6604941
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-14 03:54:14 +0000

    we

--- CommonContent/MainSideContent.php
+++ CommonContent/MainSideContent.php
@@ -116,20 +116,20 @@
             </a>
         </li>
         <li>
-            <!-- <a href="category.php">
+            <a href="category.php">
                 <i class="glyphicon glyphicon-level-up"></i><span>Category</span>
-            </a> -->
+            </a>
         </li>
         <li>
             <a href="class.php">
                 <i class="fa fa-sticky-note-o"></i> <span>Class</span>
             </a>
         </li>
-        <li>
+        <!-- <li>
             <a href="schoolyr.php">
                 <i class="fa fa-calendar-check-o"></i> <span> School Year</span>
             </a>
-        </li>
+        </li> -->
         <li>
             <a href="reports.php">
                 <i class="fa fa-bar-chart"></i> <span>Reports</span>
