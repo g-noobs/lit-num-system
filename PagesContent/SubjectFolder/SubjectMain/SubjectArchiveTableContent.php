778cec57888a6ee95e506a016eddab17c27124ec
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-14 05:42:58 +0000

    xfg

--- PagesContent/SubjectFolder/SubjectMain/SubjectArchiveTableContent.php
+++ PagesContent/SubjectFolder/SubjectMain/SubjectArchiveTableContent.php
@@ -56,3 +56,17 @@
 
 <!-- script to activate  -->
 <?php include_once "../ScriptSubj/ActivateScript.php"?>
+
+<!-- Jquery for Search -->
+<script>
+$(document).ready(function() {
+    $("#userInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("tbody tr").filter(function() {
+            var rowText = $(this).text().toLowerCase();
+            var pText = $(this).find("p").text().toLowerCase();
+            $(this).toggle(rowText.indexOf(value) > -1 || pText.indexOf(value) > -1);
+        });
+    });
+});
+</script>
