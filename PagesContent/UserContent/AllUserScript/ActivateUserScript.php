79d7256e02f1b2005406df18bc51d3823bb85cea
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-14 20:18:37 +0800

    asd

--- PagesContent/UserContent/AllUserScript/ActivateUserScript.php
+++ PagesContent/UserContent/AllUserScript/ActivateUserScript.php
@@ -24,7 +24,7 @@ $(document).ready(function() {
             $('#confirm_activate').on('click', function() {
                 //Ajax code
                 var action_url =
-                    "../PagesContent/ClassContent/ActionFolder/ActivateArchiveClassAction.php";
+                    "../PagesContent/UserContent/ActionsUsers/ActivateArchiveTeacherAction.php";
 
                 $.ajax({
                     type: "POST",
