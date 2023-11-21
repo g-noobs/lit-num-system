f29d83e56c79acf69c1bb48b734d290b4b19bfd5
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-21 02:20:06 +0000

    xcv

--- PagesContent/ReportsFolder/TableReportFolder/StudentData.php
+++ PagesContent/ReportsFolder/TableReportFolder/StudentData.php
@@ -55,5 +55,5 @@
 
 
 <!-- Common Script with other pages -->
-<?php include_once "../../../CommonContent/CommonAllScript.php"?>
+<?php include_once "../CommonContent/CommonAllScript.php"?>
 <?php include_once "../ScriptReportFolder/ExportScript.php"?>
