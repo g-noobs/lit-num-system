bfedcf949ffed05e3c595cc9e08281e85d8104dc
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-24 21:49:02 +0800

    asd

--- PagesContent/ProfileContent/PopulateDataScript.php
+++ PagesContent/ProfileContent/PopulateDataScript.php
@@ -20,7 +20,12 @@ $(function() {
             $last_name.val(responseData.last_name);
             $first_name.val(responseData.first_name);
             $middle_initial.val(responseData.middle_name);
-            $gender.val(responseData.gender);
+            $gender.find('option').each(function() {
+                if ($(this).val() === response.gender) {
+                    $(this).prop('selected', true);
+                    return false;
+                }
+            });
             $contact.val(responseData.contact_num);
             $email.val(responseData.email);
             $username.val(responseData.username);
