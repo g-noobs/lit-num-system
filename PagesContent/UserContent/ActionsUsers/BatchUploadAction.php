ed5337f34ca49d9874eb6934aa4dcee41e5996d3
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-11-25 04:11:53 +0000

    asd

--- PagesContent/UserContent/ActionsUsers/BatchUploadAction.php
+++ PagesContent/UserContent/ActionsUsers/BatchUploadAction.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 require '../../../vendor/autoload.php'; // Include PhpSpreadsheet library autoloader
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
