7db35798b8b5e4db7b1ffcab323a2d093e4d80e1
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-28 21:48:46 +0800

    asdasd

--- Database/DisplayAssignClass.php
+++ Database/DisplayAssignClass.php
@@ -22,7 +22,7 @@ class DisplayAssignClass extends Connection{
     function displayModuleList(){
         $sql = "SELECT module_id, module_name FROM tbl_module WHERE module_status = 1 AND
             NOT EXISTS 
-            ( SELECT 1 FROM tbl_teacher_module_assignment WHERE tbl_teacher_module_assignment.module_id = tbl_module.module_id AND status = 1);";
+            ( SELECT 1 FROM tbl_teacher_module_assignment WHERE tbl_teacher_module_assignment.module_id = tbl_module.module_id AND status = 0);";
         $result = $this->conn->query($sql);
         if($result->num_rows > 0){
             while($row = $result->fetch_assoc()){
