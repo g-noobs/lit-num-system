916c0f97e304f1207476cdc260aeb844d438b179
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-11-29 03:53:01 +0000

    cv

--- PagesContent/UserContent/AllUserScript/EditUserScript.php
+++ PagesContent/UserContent/AllUserScript/EditUserScript.php
@@ -78,20 +78,20 @@ $(function() {
                             console.log(error);
                         });
 
-                        setTimeout(function() {
-                            $("#add_user_modal_alert").fadeOut("slow");
+                        // setTimeout(function() {
+                        //     $("#add_user_modal_alert").fadeOut("slow");
 
-                        }, 3500);
+                        // }, 3500);
                     } else {
                         // Check if the form submission was successful
                         if (response.hasOwnProperty('success')) {
                             $hideModal.modal('hide');
                             $('#successAlert').text(response.success);
                             $('#successBanner').show();
-                            setTimeout(function() {
-                                $("#successBanner").fadeOut("slow");
-                                location.reload();
-                            }, 1500);
+                            // setTimeout(function() {
+                            //     $("#successBanner").fadeOut("slow");
+                            //     location.reload();
+                            // }, 1500);
 
 
 
@@ -99,10 +99,10 @@ $(function() {
                             $hideModal.modal('hide');
                             $('#errorAlert').text(response.error);
                             $('#errorBanner').show();
-                            setTimeout(function() {
-                                $("#errorBanner").fadeOut("slow");
-                                location.reload();
-                            }, 1500);
+                            // setTimeout(function() {
+                            //     $("#errorBanner").fadeOut("slow");
+                            //     location.reload();
+                            // }, 1500);
                         }
                     }
                 },
@@ -113,10 +113,10 @@ $(function() {
                     $('#errorAlert').text(
                         'An error occurred during the AJAX request.');
                     $('#errorBanner').show();
-                    setTimeout(function() {
-                        $("#errorBanner").fadeOut("slow");
-                        location.reload();
-                    }, 1500);
+                    // setTimeout(function() {
+                    //     $("#errorBanner").fadeOut("slow");
+                    //     location.reload();
+                    // }, 1500);
                 }
             });
         });
