0f8c3e5b90bba1a60551fc244033ae532cc448af
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-01 21:19:09 +0800

    aasd

--- PagesContent/UserContent/ActionsUsers/ActionRemoveAssignedClass.php
+++ PagesContent/UserContent/ActionsUsers/ActionRemoveAssignedClass.php
@@ -2,7 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include_once "../../../Database/SanitizeCrudClass.php";
-$conn = $connection->getConnection();
 
 $class_assign_teacher_id = $_POST['id'];
 $table = "tbl_teacher_class_assignment";
