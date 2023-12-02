b7ab95dc16b22b9103f3aed281c4efc70cff5ba7
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-12-02 02:47:14 +0000

    xcv

--- PagesContent/SubjectFolder/ScriptSubj/EditSubjScript.php
+++ PagesContent/SubjectFolder/ScriptSubj/EditSubjScript.php
@@ -1,3 +1,9 @@
 <script>
-    
+$(function() {
+    $('.edit').on('click', function(e){
+        e.preventDefault();
+        $modal = $('#add-subj');
+        $modal.modal('show');
+    });
+});
 </script>
