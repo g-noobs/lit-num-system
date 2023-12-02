99a18fa76aad8006a6ff646c9cb85f7283834b1e
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-12-02 03:13:00 +0000

    xcv

---
+++ PagesContent/SubjectFolder/ActionSubj/ActionPopulateSubjData.php
@@ -0,0 +1,31 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include_once "../../../Database/Connection.php";
+
+$connection = new Connection();
+$conn = $connection->getConnection();
+$table = "tbl_module";
+$module_id = $_POST['id'];
+
+if(!empty($module_id)){
+    $sql = "SELECT * FROM $table WHERE module_id = '$module_id';";
+    $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $response = array(
+                'module_name' => $row['module_name'],
+                'module_descrip' => $row['module_description']
+            );
+        }else{
+            $response = array('error' => 'Table Empty');
+        }
+        echo json_encode($response);
+        exit();
+}else{
+    $response = array('error' => 'No id received');
+    echo json_encode($response);
+    exit();
+}
+
+?>
--- PagesContent/SubjectFolder/ScriptSubj/EditSubjScript.php
+++ PagesContent/SubjectFolder/ScriptSubj/EditSubjScript.php
@@ -4,6 +4,26 @@ $(function() {
         e.preventDefault();
         $modal = $('#add-subj');
         $modal.modal('show');
+        var btn_id = $(this).data('id');
+        
+        $module_name = $('input[name="subj_name_add"]');
+        $module_descrip = $('textarea[name="subj_add_desc"]');
+
+        $.ajax({
+            type: "POST",
+            url: "../PagesContent/SubjectFolder/ActionSubj/ActionPopulateSubjData.php",
+            data:{
+                id: btn_id
+            },
+            dataType: 'json',
+            success: function(response) {
+                $module_name.val(response.module_name);
+                $module_descrip.val(response.module_descrip);
+            },
+            error: function() {
+                console.log('error');
+            }
+        });
     });
 });
 </script>
