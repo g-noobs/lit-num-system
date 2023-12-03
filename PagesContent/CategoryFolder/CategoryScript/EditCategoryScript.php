455b52a0fc3c9d92d3e2b8c697bc0a4334013f57
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-03 19:23:18 +0800

    xcv

--- PagesContent/CategoryFolder/CategoryScript/EditCategoryScript.php
+++ PagesContent/CategoryFolder/CategoryScript/EditCategoryScript.php
@@ -30,69 +30,70 @@ $(function() {
         e.preventDefault();
         var formData = new FormData(this);
         formData.append('id', btn_id);
-        
-    });
-    $.ajax({
-        url: '../PagesContent/CategoryFolder/ActionCategory/ActionEditCategory.php',
-        type: "POST",
-        data: formData,
-        processData: false,
-        contentType: false,
-        dataType: 'json',
-        success: function(response) {
-            // Check if the response contains an array of errors
-            if (Array.isArray(response)) {
-                // Clear previous error messages
-                $("#add_user_modal_alert_text").empty();
-                $("#add_user_modal_alert").show();
+        var actionUrl = '../PagesContent/CategoryFolder/ActionCategory/ActionEditCategory.php';
+        $.ajax({
+            url: actionUrl,
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            dataType: 'json',
+            success: function(response) {
+                // Check if the response contains an array of errors
+                if (Array.isArray(response)) {
+                    // Clear previous error messages
+                    $("#add_user_modal_alert_text").empty();
+                    $("#add_user_modal_alert").show();
 
-                // Update the element with the received errors
-                $.each(response, function(index, error) {
-                    $("#add_user_modal_alert_text").append(
-                        "<p class='error'>" +
-                        error +
-                        "</p><br>");
-                    console.log(error);
-                });
+                    // Update the element with the received errors
+                    $.each(response, function(index, error) {
+                        $("#add_user_modal_alert_text").append(
+                            "<p class='error'>" +
+                            error +
+                            "</p><br>");
+                        console.log(error);
+                    });
 
-                setTimeout(function() {
-                    $("#add_user_modal_alert").fadeOut("slow");
-
-                }, 8500);
-            } else {
-                if (response.hasOwnProperty('success')) {
-                    $("#add_category_form")[0].reset();
-                    $modal.modal('hide');
-                    $('#successAlert').text(response.success);
-                    $('#successBanner').show();
                     setTimeout(function() {
-                        $("#successBanner").fadeOut("slow");
-                        location.reload();
-                    }, 1500);
-                } else if (response.hasOwnProperty('error')) {
-                    $("#add_category_form")[0].reset();
-                    $modal.modal('hide');
-                    $('#errorAlert').text(response.error);
-                    $('#errorBanner').show();
-                    setTimeout(function() {
-                        $("#errorBanner").fadeOut("slow");
-                        location.reload();
-                    }, 1500);
+                        $("#add_user_modal_alert").fadeOut("slow");
+
+                    }, 8500);
+                } else {
+                    if (response.hasOwnProperty('success')) {
+                        $("#add_category_form")[0].reset();
+                        $modal.modal('hide');
+                        $('#successAlert').text(response.success);
+                        $('#successBanner').show();
+                        setTimeout(function() {
+                            $("#successBanner").fadeOut("slow");
+                            location.reload();
+                        }, 1500);
+                    } else if (response.hasOwnProperty('error')) {
+                        $("#add_category_form")[0].reset();
+                        $modal.modal('hide');
+                        $('#errorAlert').text(response.error);
+                        $('#errorBanner').show();
+                        setTimeout(function() {
+                            $("#errorBanner").fadeOut("slow");
+                            location.reload();
+                        }, 1500);
+                    }
                 }
+            },
+            error: function(xhr, status, error) {
+                console.error(xhr.responseText);
+                $modal.modal('hide');
+                //show alert banner id = errorBanner
+                $('#errorAlert').text(
+                    'An error occurred during the AJAX request.');
+                $('#errorBanner').show();
+                setTimeout(function() {
+                    $("#errorBanner").fadeOut("slow");
+                    location.reload();
+                }, 8500);
             }
-        },
-        error: function(xhr, status, error) {
-            console.error(xhr.responseText);
-            $modal.modal('hide');
-            //show alert banner id = errorBanner
-            $('#errorAlert').text(
-                'An error occurred during the AJAX request.');
-            $('#errorBanner').show();
-            setTimeout(function() {
-                $("#errorBanner").fadeOut("slow");
-                location.reload();
-            }, 8500);
-        }
+        });
     });
+
 });
 </script>
