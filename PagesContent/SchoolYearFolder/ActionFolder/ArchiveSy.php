e5348ae908bb618384f1295e7ef773c0363cd99d
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-05 23:52:54 +0800

    asd

--- PagesContent/SchoolYearFolder/ActionFolder/ArchiveSy.php
+++ PagesContent/SchoolYearFolder/ActionFolder/ArchiveSy.php
@@ -13,30 +13,30 @@ $sy_id =  $_POST['id'];
 
 try{
     // Step 1: Archive the school year in tbl_school_year
-    $archive_sy_query = "UPDATE tbl_schoolyear 
-    SET 
-        sy_status = 0
-    WHERE sy_id = ?";
-    $archive_sy_stmt = $conn->prepare($archive_sy_query);
-    $archive_sy_stmt->bind_param("i", $sy_id);
-    $archive_sy_stmt->execute();
+    // $archive_sy_query = "UPDATE tbl_schoolyear 
+    // SET 
+    //     sy_status = 0
+    // WHERE sy_id = ?";
+    // $archive_sy_stmt = $conn->prepare($archive_sy_query);
+    // $archive_sy_stmt->bind_param("i", $sy_id);
+    // $archive_sy_stmt->execute();
 
     // Step 2: Archive the class that has reference to the school year
-    $archive_class_query = "UPDATE tbl_class
-    SET 
-        class_status = 0
-    WHERE sy_id = ?";
-    $archive_class_stmt = $conn->prepare($archive_class_query);
-    $archive_class_stmt->bind_param("i", $sy_id);
-    $archive_class_stmt->execute();
+    // $archive_class_query = "UPDATE tbl_class
+    // SET 
+    //     class_status = 0
+    // WHERE sy_id = ?";
+    // $archive_class_stmt = $conn->prepare($archive_class_query);
+    // $archive_class_stmt->bind_param("i", $sy_id);
+    // $archive_class_stmt->execute();
 
-    // Step 3: Archive the user that has reference to the of the archived class
-    $archive_user_query = "UPDATE tbl_user_info 
-    SET status_id = 0 WHERE 
-    class_id IN (SELECT class_id FROM tbl_class WHERE sy_id = ?)";
-    $archive_user_query = $conn->prepare($archive_user_query);
-    $archive_user_query->bind_param("i", $sy_id);
-    $archive_user_query->execute();
+    // // Step 3: Archive the user that has reference to the of the archived class
+    // $archive_user_query = "UPDATE tbl_user_info 
+    // SET status_id = 0 WHERE 
+    // class_id IN (SELECT class_id FROM tbl_class WHERE sy_id = ?)";
+    // $archive_user_query = $conn->prepare($archive_user_query);
+    // $archive_user_query->bind_param("i", $sy_id);
+    // $archive_user_query->execute();
 
     //step 4: Archive the module that has reference to the school year
 
