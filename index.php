d01ed6fa1553be553746de7d314220ca9ad50647
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-05 18:55:27 +0800

    asdgg

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ ini_set('display_errors', 1);
 
     <div class="container-fluid" id="first-page">
         <div class="row">
-            <div id="left-first-page" class="col-xs-6">
+            <div class="col-xs-6">
                 <h1 id="h1-first-page"> Learn Tagakaulo <br> with <span class="emphasis">Ka-Ede</span> </h1>
                 <p> Nurture yourself with the rich tribal language using Ka-ede <br> to seamlessly learn and understand
                     the
@@ -39,7 +39,7 @@ ini_set('display_errors', 1);
                 </form>
             </div>
 
-            <div id="right-first-page" class="col-xs-6">
+            <div class="col-xs-6">
                 <img src="https://www.tagakauloedu.com/logo.png" alt="logo" id="hero-image" class="pull-right">
             </div>
         </div>
