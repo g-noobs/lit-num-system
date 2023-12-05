eebea769e1a6197f14d9733fe221aecd90796776
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-05 18:28:03 +0800

    asd

--- index.php
+++ index.php
@@ -26,8 +26,8 @@ ini_set('display_errors', 1);
         </button>
     </header>
 
-    <div class="page" id="first-page">
-        <div class="row container-fluid">
+    <div class="page container-fluid" id="first-page">
+        <div class="row">
             <div id="left-first-page" class="col-xs-6">
                 <h1 id="h1-first-page"> Learn Tagakaulo <br> with <span class="emphasis">Ka-Ede</span> </h1>
                 <p> Nurture yourself with the rich tribal language using Ka-ede <br> to seamlessly learn and understand
